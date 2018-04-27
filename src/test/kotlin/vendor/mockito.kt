95a6e6704592bccd7e40fcb3fbd7c477f6aaa71f
Fedor Muratov <muratovfyodor@yandex.ru>
2018-04-27 19:30:58 +0300

    * Add mockito as a mock framework * Add vendor tests for that

---
+++ src/test/kotlin/vendor/mockito.kt
@@ -0,0 +1,24 @@
+package vendor
+
+import com.nhaarman.mockito_kotlin.any
+import com.nhaarman.mockito_kotlin.doReturn
+import com.nhaarman.mockito_kotlin.mock
+import org.junit.Assert
+import org.junit.Test
+
+interface Dependency{
+    fun foo(a:Int): String
+}
+
+class MockitoUsage {
+    @Test
+    fun mockitoUsage() {
+        val mock = mock<Dependency>{
+            on {foo(any())} doReturn "2"
+            on {foo(1)} doReturn "1"
+        }
+
+        Assert.assertEquals("1", mock.foo(1))
+        Assert.assertEquals("2", mock.foo(555))
+    }
+}
