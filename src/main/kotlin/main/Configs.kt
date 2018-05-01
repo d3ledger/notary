f3b722ec82b0b57e163ad45725fffbcec11ffb2f
Alexey Chernyshov <alexey.n.chernyshov@gmail.com>
2018-05-01 09:28:44 +0300

    add configs stub
    Signed-off-by: Alexey Chernyshov <alexey.n.chernyshov@gmail.com>

---
+++ src/main/kotlin/main/Configs.kt
@@ -0,0 +1,10 @@
+package main
+
+/**
+ * Class incapsulates all configuration parameters for the application.
+ */
+object Configs {
+
+    /** Port for refund REST API */
+    var refundPort = 8080
+}
