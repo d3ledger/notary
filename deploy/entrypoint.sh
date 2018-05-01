4c724e26f5cdd3603719017c6bddf68e7b833f2a
Dumitru <dimasavva17@gmail.com>
2018-05-02 00:44:28 +0200

    Deploy private Ethereum network

---
+++ deploy/entrypoint.sh
@@ -0,0 +1,9 @@
+NODE="$1"
+echo ${NODE}
+/parity/parity  \
+        account import --chain /eth/genesis.json --keys-path /tmp/parity${NODE}/keys /eth/keys
+
+/parity/parity account --chain /eth/genesis.json list
+
+/parity/parity \
+        --config /eth/config/node${NODE}.toml ui 
