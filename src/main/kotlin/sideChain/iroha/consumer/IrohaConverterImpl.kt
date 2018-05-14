11e17ba743aca2325518a0cbdfa6fdf9ddffbdba
Alexey Chernyshov <alexey.n.chernyshov@gmail.com>
2018-05-14 18:43:02 +0300

    add latest precompiled bindings for mac
    Signed-off-by: Alexey Chernyshov <alexey.n.chernyshov@gmail.com>

--- src/main/kotlin/sideChain/iroha/consumer/IrohaConverterImpl.kt
+++ src/main/kotlin/sideChain/iroha/consumer/IrohaConverterImpl.kt
@@ -25,7 +25,6 @@ class IrohaConverterImpl {
                 var txBuilder = ModelTransactionBuilder()
                     .creatorAccountId(transaction.creator)
                     .createdTime(BigInteger.valueOf(System.currentTimeMillis()))
-                    .txCounter(BigInteger.valueOf(1))
 
                 for (cmd in transaction.commands) {
                     when (cmd) {
