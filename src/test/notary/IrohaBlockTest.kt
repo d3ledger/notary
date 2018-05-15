6c06d9b0ed43a43c2d3a52d138ec122c05b2cfbd
Dumitru <dimasavva17@gmail.com>
2018-05-15 14:15:31 +0300

    Iroha block test
    Signed-off-by: Dumitru <dimasavva17@gmail.com>

--- src/test/notary/IrohaBlockTest.kt
+++ src/test/notary/IrohaBlockTest.kt
@@ -1,24 +1,21 @@
 package notary
 
-import com.squareup.moshi.KotlinJsonAdapterFactory
-import com.squareup.moshi.Moshi
+
+import org.junit.jupiter.api.Assertions.assertEquals
 import org.junit.jupiter.api.Test
-import sideChain.iroha.Block
 import java.io.File
+
 
 class IrohaBlockTest {
     @Test
-    fun jsonToBlock() {
-        val file = File("resources/genesis.block")
-        val json = file.readText()
-        val moshi = Moshi.Builder()
-                .add(KotlinJsonAdapterFactory())
-                .build()
-        val jsonAdapter = moshi.adapter(Block::class.java)
+    fun parseProtoBlock() {
+        val file = File("resources/genesis.bin")
+        val bs = file.readBytes()
+        val bl = iroha.protocol.BlockOuterClass.Block.parseFrom(bs)
+        val txs = bl.payload.transactionsList
+        val cmds = txs.flatMap { it.payload.commandsList }
 
-
-        val block = jsonAdapter.fromJson(json)
-
-        println(block)
+        assertEquals(1, txs.size)
+        assertEquals(10, cmds.size)
     }
 }
