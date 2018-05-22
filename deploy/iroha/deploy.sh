d9e0d7b663013ce4fc85b1e93b19069a79533879
Dumitru <dimasavva17@gmail.com>
2018-05-22 12:57:05 +0300

    Remove possible name clashing
    Signed-off-by: Dumitru <dimasavva17@gmail.com>

--- deploy/iroha/deploy.sh
+++ deploy/iroha/deploy.sh
@@ -2,24 +2,25 @@
 
 CURDIR="$(cd "$(dirname "$0")"; pwd)"
 
-docker rm -f iroha
-docker rm -f some-postgres
-docker network create iroha-network || true
+docker rm -f d3ledger-iroha
+docker rm -f d3ledger-postgres
+docker network create d3ledger-iroha-network || true
 
-docker run --name some-postgres \
+docker run --name d3ledger-postgres \
     -e POSTGRES_USER=postgres \
     -e POSTGRES_PASSWORD=mysecretpassword \
-    --network=iroha-network \
+    --network=d3ledger-iroha-network \
     -d postgres:9.5
 
 echo "Wait 5 seconds to start Postgres"
 sleep 5
 
-docker volume create blockstore
-docker run -it --name iroha \
+docker volume create d3ledger-blockstore
+
+docker run -it --name d3ledger-iroha \
 -p 50051:50051 \
 -v ${CURDIR}:/opt/iroha_data \
--v blockstore:/tmp/block_store \
---network=iroha-network \
+-v d3ledger-blockstore:/tmp/block_store \
+--network=d3ledger-iroha-network \
 --entrypoint="" \
 hyperledger/iroha:develop bash -c "irohad --config config.docker --genesis_block genesis.block --keypair_name keys/admin\@test"
