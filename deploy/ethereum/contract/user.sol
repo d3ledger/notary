3af2a43fb18aa7de5e2e0d67f6cebbac27b34a60
luckychess <toobwn@gmail.com>
2018-05-29 13:27:11 +0300

    Add stub for transit smart contract.
    Signed-off-by: luckychess <toobwn@gmail.com>

---
+++ deploy/ethereum/contract/user.sol
@@ -0,0 +1,26 @@
+pragma solidity ^0.4.23;
+
+contract User {
+    address private owner_;
+    address private master_;
+
+    constructor(address master) public {
+        owner_ = msg.sender;
+        master_ = master;
+    }
+
+    function() public payable { }
+
+    function sendAllToMaster() private {
+        master_.transfer(address(this).balance);
+        // loop by token addresses and transfer all of them to master address
+    }
+
+    function withdrawEther() public {
+        // send everything to master account
+    }
+
+    function withdrawTokens() public {
+        // send everything to master account
+    }
+}
