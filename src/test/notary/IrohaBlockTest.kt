69f3f906b6a8c7e02a1870ceddd8cb15476afff3
Dumitru <dimasavva17@gmail.com>
2018-05-31 17:46:34 +0300

    Block fromProto test
    Signed-off-by: Dumitru <dimasavva17@gmail.com>

--- src/test/notary/IrohaBlockTest.kt
+++ src/test/notary/IrohaBlockTest.kt
@@ -1,8 +1,10 @@
 package notary
 
 
+import org.junit.jupiter.api.Assertions.assertArrayEquals
 import org.junit.jupiter.api.Assertions.assertEquals
 import org.junit.jupiter.api.Test
+import sideChain.iroha.IrohaBlockStub
 import java.io.File
 
 class IrohaBlockTest {
@@ -11,11 +13,21 @@ class IrohaBlockTest {
         val file = File("resources/genesis.bin")
         val bs = file.readBytes()
         val bl = iroha.protocol.BlockOuterClass.Block.parseFrom(bs)
+        val block = IrohaBlockStub.fromProto(bs)
         val txs = bl.payload.transactionsList
         val cmds = txs.flatMap { it.payload.commandsList }
 
+        val hash = ByteArray(32)
+        hash.fill(0)
+
+        val command = block.transactions.first().commands.first() as IrohaCommand.CommandAddPeer
         assertEquals(1, txs.size)
         assertEquals(10, cmds.size)
+        assertEquals(1, block.txNumber)
+        assertEquals(1, block.height)
+        assertEquals("localhost:10001", command.address)
+        assertArrayEquals(hash, block.prevBlockHash)
+
     }
 
 }
