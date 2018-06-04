00ff82263abff77d5b1655f1e47a81bfbd7e6803
Dumitru <dimasavva17@gmail.com>
2018-06-04 17:08:09 +0300

    Remove UI, fix paths
    Signed-off-by: Dumitru <dimasavva17@gmail.com>

--- deploy/ethereum/deploy.sh
+++ deploy/ethereum/deploy.sh
@@ -6,7 +6,7 @@ docker rm -f node0 || true
 docker rm -f node1 || true
 docker rm -f manager || true
 
-docker network create eth || true
+docker network create d3ledger-eth-network || true
 
 # Run node0
 docker run -d -v \
@@ -16,14 +16,14 @@ docker run -d -v \
 	-p 8545:8545 \
     -p 8546:8546 \
 	-p 8180:8180 \
-	--network eth \
+	--network d3ledger-eth-network \
 	--entrypoint ""  parity/parity  bash -c "chmod +x /eth/entrypoint.sh && /eth/entrypoint.sh 0"
 
 # Run node1
 docker run -d -v \
     ${CURDIR}:/eth \
     --name node1 \
-    --network eth \
+    --network d3ledger-eth-network \
 	--entrypoint "" \
 	parity/parity  bash -c "chmod +x /eth/entrypoint.sh && /eth/entrypoint.sh 1"
 
@@ -33,6 +33,6 @@ sleep 5
 docker run -it \
     -v ${CURDIR}:/eth \
     --name manager \
-    --network eth \
+    --network d3ledger-eth-network \
     --entrypoint "" \
     python bash -c "pip install requests && python /eth/main.py"
