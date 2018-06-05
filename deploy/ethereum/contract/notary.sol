3abf646d061a5dad22f8accc54de560ffaf9a267
luckychess <toobwn@gmail.com>
2018-06-05 12:22:37 +0300

    Implement core contract functionality:
    - Create user contract as an endpoint for end users
    - Use genuine signature verification by peers added via AddPeer
    - Add lots of events for every movement for debug purposes
    - Add support for ether
    
    Signed-off-by: luckychess <toobwn@gmail.com>

--- deploy/ethereum/contract/notary.sol
+++ deploy/ethereum/contract/notary.sol
@@ -1,20 +1,17 @@
 pragma solidity ^0.4.23;
 
 contract BasicCoin {
-    function transfer(address _to, uint256 _value) public returns (bool);
-    function approve(address _spender, uint256 _value) public returns (bool);
-    function balanceOf(address _who) public constant returns (uint256);
+    function transfer(address to, uint256 value) public returns (bool);
+    function approve(address spender, uint256 value) public returns (bool);
+    function balanceOf(address who) public constant returns (uint256);
 }
     
 contract Notary {
-    struct Peer {
-        bytes32 public_key_;
-    }
-
-    address private owner_;
-    bytes32[] private peers_;
-    
     BasicCoin private bc_;
+    address private owner_;
+    mapping(address => bool) private peers_;
+    uint private peers_count_;
+    mapping(bytes32 => bool) private used_;
 
     event address_event(address input);
     event string_event(string input);
@@ -27,10 +24,7 @@ contract Notary {
 
     function() public payable { }
 
-    function verify(bytes32 hash, uint8 v, bytes32 r, bytes32 s) public returns(address) {
-        // Note: this only verifies that signer is correct.
-        // You'll also need to verify that the hash of the data
-        // is also correct.
+    function verify(bytes32 hash, uint8 v, bytes32 r, bytes32 s) private returns(address) {
         emit bytes_event(hash);
         bytes32 simple_hash = keccak256(abi.encodePacked("\x19Ethereum Signed Message:\n32", hash));
         emit bytes_event(simple_hash);
@@ -40,35 +34,64 @@ contract Notary {
         return res;
     }
     
-    function addPeer(bytes32 public_key) public {
+    function addPeer(address new_address) public {
         if (msg.sender == owner_) {
-            peers_.push(public_key);
+            peers_[new_address] = true;
+            ++peers_count_;
         }
     }
-    
-    function getPeers() constant public returns (bytes32[]) {
-        return peers_;
+
+    function checkForUniqueness(address[] memory addresses) pure internal returns(bool) {
+        for (uint i = 0; i < addresses.length; ++i) {
+            for (uint j = i+1; j < addresses.length; ++j) {
+                if (addresses[i] == addresses[j]) {
+                    return false;
+                }
+            }
+        }
+        return true;
     }
 
-    function receiveTokens(/*string metadata*/) public pure {
-        // TODO: call method from ERC-20 smart contract
-        // or don't call
-        // depends on what we will decide to do - registration, double tx or whatever
-    }
+    function withdraw(address coin_address, uint256 amount, address to, bytes32 tx_hash, uint8 []v, bytes32 []r, bytes32 []s) public {
+        // TODO: token whitelist check? (similar to user contract)
+        // TODO: improve require checks (copy-paste) (use modifiers)
+        require(used_[tx_hash] == false);
+        require(peers_count_ >= 1);
+        // sigs - at least 2f+1 from 3f+1
+        // e. g. len(peers)==12 -> 3f+1=12; f=3; 12-3=9 -> at least 9 sigs we need
+        // if we've got more sigs than we need all will be validated
+        uint f = (peers_count_ - 1) / 3;
+        uint need_sigs = peers_count_ - f;
+        emit number_event(need_sigs);
 
-    function withdrawTokens(address coin_address, uint256 amount, address to, bytes32 hash, uint8 v, bytes32 r, bytes32 s) public {
-        // TODO: sigs - at least 2f+1 from 3f+1
-        // len(peers)==12 -> 3f+1=12; f=3; 2f+1=7 -> at least 7 sigs we need
-        // type (address of token for now)
-        // nonce (think)
-        address res = verify(hash, v, r, s);
-        // for now only requests signed by owner can be satisfied
-        assert(res == owner_);
-        bc_ = BasicCoin(coin_address);
-        emit address_event(coin_address);
-        emit address_event(this);
-        emit number_event(bc_.balanceOf(this));
-        assert(bc_.balanceOf(this) >= amount);
-        bc_.transfer(to, amount);
+        assert(v.length == r.length);
+        assert(r.length == s.length);
+        assert(s.length >= need_sigs);
+        emit number_event(s.length);
+
+        address[] memory recovered_addresses = new address[](s.length);
+        for (uint i = 0; i < s.length; ++i) {
+            if (coin_address == 0) {
+                recovered_addresses[i] = verify(keccak256(abi.encodePacked(amount, to, tx_hash)), v[i], r[i], s[i]);
+            } else {
+                recovered_addresses[i] = verify(keccak256(abi.encodePacked(coin_address, amount, to, tx_hash)), v[i], r[i], s[i]);
+            }
+            // recovered address should be in peers_
+            assert(peers_[recovered_addresses[i]] == true);
+        }
+        assert(checkForUniqueness(recovered_addresses));
+
+        if (coin_address == 0) {
+            emit number_event(address(this).balance);
+            assert(address(this).balance >= amount);
+            to.transfer(amount);
+        } else {
+            bc_ = BasicCoin(coin_address);
+            emit address_event(coin_address);
+            emit number_event(bc_.balanceOf(this));
+            assert(bc_.balanceOf(this) >= amount);
+            bc_.transfer(to, amount);
+        }
+        used_[tx_hash] = true;
     }
 }
--- deploy/ethereum/contract/user.sol
+++ deploy/ethereum/contract/user.sol
@@ -1,12 +1,30 @@
 pragma solidity ^0.4.23;
 
-contract User {
-    address private owner_;
-    address private master_;
+contract ICoin {
+    function transfer(address to, uint256 value) public returns (bool);
+    function balanceOf(address who) public constant returns (uint256);
+}
 
-    constructor(address master) public {
-        owner_ = msg.sender;
+contract INotary {
+    function withdraw(address coin_address, uint256 amount, address to, bytes32 tx_hash, uint8 []v, bytes32 []r, bytes32 []s) public;
+}
+
+contract User {
+    address private master_;
+    address[] private tokens_;
+    INotary private in_;
+
+    event address_event(address input);
+    event string_event(string input);
+    event bytes_event(bytes32 input);
+    event number_event(uint256 input);
+
+    constructor(address master, address[] tokens) public {
         master_ = master;
+        for (uint i = 0; i < tokens.length; ++i) {
+            tokens_.push(tokens[i]);
+        }
+        in_ = INotary(master_);
     }
 
     // can accept ether
@@ -14,14 +32,31 @@ contract User {
 
     function sendAllToMaster() private {
         master_.transfer(address(this).balance);
-        // loop by token addresses and transfer all of them to master address
+        // loop through all token addresses and transfer all tokens to master address
+        for (uint i = 0; i < tokens_.length; ++i) {
+            ICoin ic = ICoin(tokens_[i]);
+            ic.transfer(master_, ic.balanceOf(address(this)));
+        }
     }
 
-    function withdrawEther() public {
-        // send everything to master account
+    function checkTokenAddress(address token) private constant returns (bool) {
+        bool token_found = false;
+        for (uint i = 0; i < tokens_.length; ++i) {
+            if (tokens_[i] == token) {
+                token_found = true;
+                break;
+            }
+        }
+        return token_found;
     }
 
-    function withdrawTokens() public {
-        // send everything to master account
+    function withdraw(address coin_address, uint256 amount, address to, bytes32 tx_hash, uint8 []v, bytes32 []r, bytes32 []s) public {
+        if (coin_address != 0) {
+            assert(checkTokenAddress(coin_address));
+            emit address_event(coin_address);
+        }
+        sendAllToMaster();
+        emit address_event(master_);
+        in_.withdraw(coin_address, amount, to, tx_hash, v, r, s);
     }
 }
