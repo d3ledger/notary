3d70dea1d8961bdbeeea9d7c3214028f9ec39b6b
luckychess <toobwn@gmail.com>
2018-06-26 20:43:57 +0300

    Add comments to contracts code.
    Signed-off-by: luckychess <toobwn@gmail.com>

--- deploy/ethereum/contract/master.sol
+++ deploy/ethereum/contract/master.sol
@@ -1,13 +1,19 @@
 pragma solidity ^0.4.23;
 
-contract BasicCoin {
+/**
+ * Subset of ERC-20 token interface
+ */
+contract ICoin {
     function transfer(address to, uint256 value) public returns (bool);
     function approve(address spender, uint256 value) public returns (bool);
     function balanceOf(address who) public constant returns (uint256);
 }
-    
+
+/**
+ * Provides functionality of master contract
+ */
 contract Master {
-    BasicCoin private bc_;
+    ICoin private ic_;
     address private owner_;
     mapping(address => bool) private peers_;
     uint private peers_count_;
@@ -18,12 +24,26 @@ contract Master {
     event bytes_event(bytes32 input);
     event number_event(uint256 input);
 
+    /**
+     * Constructor. Sets contract owner to contract creator.
+     */
     constructor() public {
         owner_ = msg.sender;
     }
 
+    /**
+     * A special function-like stub to allow ether accepting
+     */
     function() public payable { }
 
+    /**
+     * Performs verification of a single signature
+     * @param hash unsigned data
+     * @param v v-component of signature from hash
+     * @param r r-component of signature from hash
+     * @param s s-component of signature from hash
+     * @return address recovered from signature
+     */
     function verify(bytes32 hash, uint8 v, bytes32 r, bytes32 s) private returns(address) {
         emit bytes_event(hash);
         bytes32 simple_hash = keccak256(abi.encodePacked("\x19Ethereum Signed Message:\n32", hash));
@@ -33,7 +53,11 @@ contract Master {
         emit address_event(res);
         return res;
     }
-    
+
+    /**
+     * Adds new peer to list of signature verifiers. Can be called only by contract owner.
+     * @param new_address address of new peer
+     */
     function addPeer(address new_address) public {
         if (msg.sender == owner_) {
             peers_[new_address] = true;
@@ -43,6 +67,11 @@ contract Master {
         }
     }
 
+    /**
+     * Checks given addresses for duplicates
+     * @param addresses addresses array to check
+     * @return true if all given addresses are unique or false otherwise
+     */
     function checkForUniqueness(address[] memory addresses) pure internal returns(bool) {
         for (uint i = 0; i < addresses.length; ++i) {
             for (uint j = i+1; j < addresses.length; ++j) {
@@ -54,6 +83,16 @@ contract Master {
         return true;
     }
 
+    /**
+     * Withdraws specified amount of ether or one of ERC-20 tokens to provided address
+     * @param coin_address address of token to withdraw (0 for ether)
+     * @param amount amount of tokens or ether to withdraw
+     * @param to target account address
+     * @param tx_hash hash of transaction from Iroha
+     * @param v array of signatures of tx_hash (v-component)
+     * @param r array of signatures of tx_hash (r-component)
+     * @param s array of signatures of tx_hash (s-component)
+     */
     function withdraw(address coin_address, uint256 amount, address to, bytes32 tx_hash, uint8 []v, bytes32 []r, bytes32 []s) public {
         // TODO luckychess 26.06.2018 D3-103 fix whitelist checks inconsistency
         // TODO luckychess 26.06.2018 D3-101 improve require checks (copy-paste) (use modifiers)
@@ -88,11 +127,11 @@ contract Master {
             assert(address(this).balance >= amount);
             to.transfer(amount);
         } else {
-            bc_ = BasicCoin(coin_address);
+            ic_ = ICoin(coin_address);
             emit address_event(coin_address);
-            emit number_event(bc_.balanceOf(this));
-            assert(bc_.balanceOf(this) >= amount);
-            bc_.transfer(to, amount);
+            emit number_event(ic_.balanceOf(this));
+            assert(ic_.balanceOf(this) >= amount);
+            ic_.transfer(to, amount);
         }
         used_[tx_hash] = true;
     }
--- deploy/ethereum/contract/relay.sol
+++ deploy/ethereum/contract/relay.sol
@@ -1,14 +1,23 @@
 pragma solidity ^0.4.23;
 
+/**
+ * Subset of ERC-20 token interface
+ */
 contract ICoin {
     function transfer(address to, uint256 value) public returns (bool);
     function balanceOf(address who) public constant returns (uint256);
 }
 
+/**
+ * Subset of master contract interface
+ */
 contract IMaster {
     function withdraw(address coin_address, uint256 amount, address to, bytes32 tx_hash, uint8 []v, bytes32 []r, bytes32 []s) public;
 }
 
+/**
+ * Provides functionality of relay contract
+ */
 contract Relay {
     address private master_;
     address[] private tokens_;
@@ -19,6 +28,11 @@ contract Relay {
     event bytes_event(bytes32 input);
     event number_event(uint256 input);
 
+    /**
+     * Relay constructor
+     * @param master address of master contract
+     * @param tokens whitelist of supported ERC-20 tokens
+     */
     constructor(address master, address[] tokens) public {
         master_ = master;
         for (uint i = 0; i < tokens.length; ++i) {
@@ -27,9 +41,14 @@ contract Relay {
         in_ = IMaster(master_);
     }
 
-    // can accept ether
+    /**
+     * A special function-like stub to allow ether accepting
+     */
     function() public payable { }
 
+    /**
+     * Sends ether and all tokens from this contract to master
+     */
     function sendAllToMaster() private {
         master_.transfer(address(this).balance);
         // loop through all token addresses and transfer all tokens to master address
@@ -39,6 +58,11 @@ contract Relay {
         }
     }
 
+    /**
+     * Checks is given token inside a whitelist or not
+     * @param token address of token to check
+     * @return true if token inside whitelist or false otherwise
+     */
     function checkTokenAddress(address token) private constant returns (bool) {
         bool token_found = false;
         for (uint i = 0; i < tokens_.length; ++i) {
@@ -50,6 +74,16 @@ contract Relay {
         return token_found;
     }
 
+    /**
+     * Withdraws specified amount of ether or one of ERC-20 tokens to provided address
+     * @param coin_address address of token to withdraw (0 for ether)
+     * @param amount amount of tokens or ether to withdraw
+     * @param to target account address
+     * @param tx_hash hash of transaction from Iroha
+     * @param v array of signatures of tx_hash (v-component)
+     * @param r array of signatures of tx_hash (r-component)
+     * @param s array of signatures of tx_hash (s-component)
+     */
     function withdraw(address coin_address, uint256 amount, address to, bytes32 tx_hash, uint8 []v, bytes32 []r, bytes32 []s) public {
         if (coin_address != 0) {
             assert(checkTokenAddress(coin_address));
