a5363408fba31933475b076ea3a1bd37a93cc294
Alexey Chernyshov <alexey.n.chernyshov@gmail.com>
2018-06-26 17:28:23 +0300

    add Iroha output to registration
    Signed-off-by: Alexey Chernyshov <alexey.n.chernyshov@gmail.com>

--- src/main/kotlin/registration/EthFreeWalletsProvider.kt
+++ src/main/kotlin/registration/EthFreeWalletsProvider.kt
@@ -4,6 +4,11 @@ package registration
  * Provides with free ethereum relay wallet
  */
 class EthFreeWalletsProvider {
+
+    // TODO add effective implementation
+    // Possible solutions:
+    // 1) set listener and update Postgres DB
+    // 2) query Iroha each time
     fun getWallet(): String {
         return "eth_wallet"
     }
