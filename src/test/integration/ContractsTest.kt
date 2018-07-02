3068ba0c56e71ef5ef0c58e6cbb1aac006ee287c
Konstantin Munichev <luckychess@users.noreply.github.com>
2018-07-02 14:46:05 +0300

    Implement core contract functionality (#32)
    - Create user contract as an endpoint for end users
    - Use genuine signature verification by peers added via AddPeer
    - Add lots of events for every movement for debug purposes
    - Add support for ether
    - Add simple ERC-20 token implementation
    - Add test for withdraw on master
    - Add test for repeated hash
    
    Signed-off-by: luckychess <toobwn@gmail.com>

---
+++ src/test/integration/ContractsTest.kt
@@ -0,0 +1,138 @@
+package integration
+
+import contract.BasicCoin
+import contract.Master
+import contract.Relay
+import notary.CONFIG
+import main.ConfigKeys
+import org.junit.jupiter.api.BeforeAll
+import org.junit.jupiter.api.BeforeEach
+import org.junit.jupiter.api.Test
+import org.web3j.abi.datatypes.generated.Uint256
+import org.web3j.crypto.Hash
+import org.web3j.protocol.http.HttpService
+import org.web3j.protocol.parity.Parity
+import java.math.BigInteger
+
+/**
+ * Class for Ethereum sidechain infrastructure deployment and communication.
+ */
+class ContractsTest {
+    val deploy_helper = DeployHelper()
+
+    private lateinit var token: BasicCoin
+    private lateinit var master: Master
+    private lateinit var relay: Relay
+
+    // predefined account which already exists on parity node
+    private val acc_green = "0x00Bd138aBD70e2F00903268F3Db08f2D25677C9e"
+
+    private fun hexStringToByteArray(s: String): ByteArray {
+        val len = s.length
+        val data = ByteArray(len / 2)
+        var i = 0
+        while (i < len) {
+            data[i / 2] = ((Character.digit(s[i], 16) shl 4) + Character.digit(s[i + 1], 16)).toByte()
+            i += 2
+        }
+        return data
+    }
+
+    private fun signUserData(to_sign: String): String {
+        // TODO luckychess 26.06.2018 D3-100 find a way to produce correct signatures locally
+        val parity = Parity.build(HttpService(CONFIG[ConfigKeys.ethConnectionUrl]))
+        val unlock = parity.personalUnlockAccount(deploy_helper.credentials.address, "user").send()
+        assert(unlock.accountUnlocked())
+        val signature = parity.ethSign(deploy_helper.credentials.address, to_sign).send().signature
+        println("eth_sign: $signature")
+        return signature
+    }
+
+    private fun sendAddPeer(address: String) {
+        val add_peer = master.addPeer(address).send()
+        // addPeer call produces 2 events
+        // first event is amount of peers after new peer was added
+        // second one is address of added peer
+        // events should be aligned to 64 hex digits and prefixed with 0x
+        assert(add_peer.logs.size == 2)
+        assert(add_peer.logs[0].data == "0x" + String.format("%064x", 1))
+        assert(add_peer.logs[1].data == "0x" + "0".repeat(24) +
+                address.slice(2 until address.length))
+    }
+
+    private fun transferTokensToMaster(amount: BigInteger) {
+        token.transfer(master.contractAddress, amount).send()
+        assert(token.balanceOf(master.contractAddress).send() == amount)
+    }
+
+    private fun withdraw(
+            amount: BigInteger,
+            iroha_hash: String = Hash.sha3(String.format("%064x", BigInteger.valueOf(12345)))) {
+        // it was really painful to find out how to combine fields
+        // in a same way as js and solidity do
+        val final_hash = Hash.sha3(token.contractAddress.replace("0x", "")
+                + String.format("%064x", amount).replace("0x", "")
+                + acc_green.replace("0x", "")
+                + iroha_hash.replace("0x", ""))
+        println("hash: $final_hash")
+
+        val signature = signUserData(final_hash)
+        val r = hexStringToByteArray(signature.substring(2, 66))
+        val s = hexStringToByteArray(signature.substring(66, 130))
+        val v = signature.substring(130, 132).toBigInteger(16)
+
+        val vv = ArrayList<BigInteger>()
+        vv.add(v)
+        val rr = ArrayList<ByteArray>()
+        rr.add(r)
+        val ss = ArrayList<ByteArray>()
+        ss.add(s)
+
+        val byte_hash = hexStringToByteArray(iroha_hash.slice(2 until iroha_hash.length))
+        master.withdraw(
+                token.contractAddress,
+                amount,
+                acc_green,
+                byte_hash,
+                vv,
+                rr,
+                ss).send()
+    }
+
+    @BeforeEach
+    fun setup() {
+        token = deploy_helper.deployBasicCoinSmartContract()
+        master = deploy_helper.deployMasterSmartContract()
+        relay = deploy_helper.deployRelaySmartContract(master.contractAddress, listOf(token.contractAddress))
+    }
+
+    /**
+     * @given deployed master and token contracts
+     * @when one peer added to master, 5 tokens transferred to master,
+     * request to withdraw 1 token is sent to master
+     * @then call to withdraw succeeded
+     */
+    @Test
+    fun singleCorrectSignatureTest() {
+        sendAddPeer(deploy_helper.credentials.address)
+        transferTokensToMaster(BigInteger.valueOf(5))
+        withdraw(BigInteger.valueOf(1))
+        assert(token.balanceOf(master.contractAddress).send() == BigInteger.valueOf(4))
+    }
+
+    /**
+     * @given deployed master and token contracts
+     * @when one peer added to master, 5 tokens transferred to master,
+     * request to withdraw 1 token is sent to master twice
+     * @then second call to withdraw failed
+     */
+    @Test
+    fun usedHashTest() {
+        sendAddPeer(deploy_helper.credentials.address)
+        transferTokensToMaster(BigInteger.valueOf(5))
+        withdraw(BigInteger.valueOf(1))
+        assert(token.balanceOf(master.contractAddress).send() == BigInteger.valueOf(4))
+        withdraw(BigInteger.valueOf(1))
+        assert(token.balanceOf(master.contractAddress).send() == BigInteger.valueOf(4))
+    }
+}
--- src/test/integration/IntegrationTest.kt
+++ src/test/integration/IntegrationTest.kt
@@ -21,10 +21,7 @@ import notary.main
 import org.junit.jupiter.api.fail
 import org.web3j.crypto.RawTransaction
 import org.web3j.crypto.TransactionEncoder
-import org.web3j.crypto.WalletUtils
-import org.web3j.protocol.Web3j
 import org.web3j.protocol.core.DefaultBlockParameterName
-import org.web3j.protocol.http.HttpService
 import org.web3j.utils.Numeric
 import sidechain.iroha.IrohaInitialization
 import sidechain.iroha.consumer.IrohaKeyLoader
@@ -37,7 +34,6 @@ import java.math.BigInteger
  * Class for Ethereum sidechain infrastructure deployment and communication.
  */
 class IntegrationTest {
-
     init {
         IrohaInitialization.loadIrohaLibrary()
             .failure {
@@ -45,6 +41,8 @@ class IntegrationTest {
                 System.exit(1)
             }
     }
+
+    private val deploy_helper = DeployHelper()
 
     /** Iroha keypair */
     val keypair: Keypair =
@@ -59,19 +57,6 @@ class IntegrationTest {
     /** Iroha transaction creator */
     val creator = CONFIG[ConfigKeys.irohaCreator]
 
-    /** web3 service instance to communicate with Ethereum network */
-    private val web3 = Web3j.build(HttpService(CONFIG[ConfigKeys.ethConnectionUrl]))
-
-    /** credentials of ethereum user */
-    private val credentials =
-        WalletUtils.loadCredentials("user", "deploy/ethereum/keys/user.key")
-
-    /** Gas price */
-    private val gasPrice = BigInteger.ONE
-
-    /** Max gas limit */
-    private val gasLimit = BigInteger.valueOf(999999)
-
     /** Ethereum address to transfer from */
     private val fromAddress = "0x004ec07d2329997267Ec62b4166639513386F32E"
 
@@ -83,18 +68,19 @@ class IntegrationTest {
      */
     private fun sendEthereum(amount: BigInteger) {
         // get the next available nonce
-        val ethGetTransactionCount = web3.ethGetTransactionCount(
-            fromAddress, DefaultBlockParameterName.LATEST
+        val ethGetTransactionCount = deploy_helper.web3.ethGetTransactionCount(
+                fromAddress, DefaultBlockParameterName.LATEST
         ).send()
         val nonce = ethGetTransactionCount.transactionCount
 
         // create our transaction
-        val rawTransaction = RawTransaction.createTransaction(nonce, gasPrice, gasLimit, toAddress, amount, "")
+        val rawTransaction = RawTransaction.createTransaction(
+                nonce, deploy_helper.gasPrice, deploy_helper.gasLimit, toAddress, amount, "")
 
         // sign & send our transaction
-        val signedMessage = TransactionEncoder.signMessage(rawTransaction, credentials)
+        val signedMessage = TransactionEncoder.signMessage(rawTransaction, deploy_helper.credentials)
         val hexValue = Numeric.toHexString(signedMessage)
-        web3.ethSendRawTransaction(hexValue).send()
+        deploy_helper.web3.ethSendRawTransaction(hexValue).send()
     }
 
     /**
@@ -185,7 +171,7 @@ class IntegrationTest {
         val queryResponse = queryStub.find(protoQuery)
 
         val fieldDescriptor =
-            queryResponse.descriptorForType.findFieldByName("account_assets_response")
+                queryResponse.descriptorForType.findFieldByName("account_assets_response")
         if (!queryResponse.hasField(fieldDescriptor)) {
             fail { "Query response error ${queryResponse.errorResponse}" }
         }
@@ -195,77 +181,6 @@ class IntegrationTest {
             println("account " + asset.accountId)
             println("asset ${asset.assetId} - ${asset.balance}")
         }
-    }
-
-
-    /**
-     * Deploy BasicCoin smart contract
-     * @return token smart contract address
-     */
-    private fun deployBasicCoinSmartContract(): String {
-        val contract =
-            contract.BasicCoin.deploy(
-                web3,
-                credentials,
-                gasPrice,
-                gasLimit
-            ).send()
-
-        return contract.contractAddress
-    }
-
-    /**
-     * Deploy notary smart contract
-     * @return notary smart contract address
-     */
-    private fun deployNotarySmartContract(): String {
-        val contract =
-            contract.Notary.deploy(
-                web3,
-                credentials,
-                gasPrice,
-                gasLimit
-            ).send()
-
-        return contract.contractAddress
-    }
-
-    /**
-     * Deploy user smart contract
-     * @param master notary master account
-     * @param tokens list of supported tokens
-     * @return user smart contract address
-     */
-    private fun deployUserSmartContract(master: String, tokens: List<String>): String {
-        val contract =
-            contract.User.deploy(
-                web3,
-                credentials,
-                gasPrice,
-                gasLimit,
-                master,
-                tokens
-            ).send()
-
-        return contract.contractAddress
-    }
-
-    /**
-     * Deploy all smart contracts:
-     * - notary
-     * - token
-     * - user
-     */
-    fun deployAll() {
-        val token = deployBasicCoinSmartContract()
-        val notary = deployNotarySmartContract()
-
-        val tokens = listOf(token)
-        val user = deployUserSmartContract(notary, tokens)
-
-        println("Token contract address: $token")
-        println("Notary contract address: $notary")
-        println("User contract address: $user")
     }
 
     /**
