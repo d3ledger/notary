d78a1efcac968a10d524b7f305ac349976141a32
Artyom Bakhtin <a@bakhtin.net>
2018-07-04 15:27:25 +0300

    add docker-compose
    Signed-off-by: Artyom Bakhtin <a@bakhtin.net>

--- deploy/ethereum/entrypoint.sh
+++ deploy/ethereum/entrypoint.sh
@@ -1,5 +1,6 @@
-NODE="$1"
-echo ${NODE}
+#!/bin/sh
+NODE=${NODE:-0}
+
 /parity/parity  \
         account import --chain /eth/genesis.json --keys-path /tmp/parity/node${NODE}/keys /eth/keys
 
---
+++ notary.dockerfile
@@ -0,0 +1,5 @@
+FROM gradle
+
+ADD --chown=gradle:gradle . /build/
+RUN gradle -b /build/build.gradle assemble
+ENTRYPOINT ["gradle", "-b", "/build/build.gradle", "runRegistration"]
