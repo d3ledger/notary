74ac6854e2803d068ebb26d6f6b2d0120e9f9fb5
Alexey <Alexey.N.Chernyshov@gmail.com>
2018-07-25 16:08:31 +0300

    fix getWallets in notary (#91)
    Signed-off-by: Alexey Chernyshov <alexey.n.chernyshov@gmail.com>

--- src/test/kotlin/notary/NotaryInitializationTest.kt
+++ src/test/kotlin/notary/NotaryInitializationTest.kt
@@ -24,9 +24,6 @@ class NotaryInitializationTest {
             }
     }
 
-    /** Some exception message */
-    val throwMessage = "Hey, look at my exception!"
-
     /** Mock iroha configs */
     val irohaConfig = mock<IrohaConfig> {
         on { hostname } doReturn "localhost"
@@ -67,54 +64,4 @@ class NotaryInitializationTest {
     /** Mock Iroha network */
     val irohaNetwork = mock<IrohaNetwork>()
 
-    /**
-     * @given eth wallets loader and notary initializer
-     * @when eth wallets loader fails with exception
-     * @then notary initializer returns Result with reason
-     */
-    @Test
-    fun testFailToLoadWallets() {
-
-        // Wallet provider that fails with exception
-        val failEthWalletProvider = mock<EthWalletsProvider> {
-            on {
-                getWallets()
-            } doReturn Result.of { throw Exception(throwMessage) }
-        }
-
-        val notaryInit =
-            NotaryInitialization(notaryConfig, passwordConfig, failEthWalletProvider, ethTokenProvider, irohaNetwork)
-
-        notaryInit.init().fold(
-            { fail { "Exception should be thrown in wallets loader" } },
-            { exc ->
-                assertEquals(throwMessage, exc.message)
-            }
-        )
-    }
-
-    /**
-     * @given eth tokens loader and notary initializer
-     * @when eth tokens loader fails with exception
-     * @then notary initializer returns Result with reason
-     */
-    @Test
-    fun testFailToLoadTokens() {
-
-        // Token provider that fails with exception
-        val failEthTokenProvider = mock<EthTokensProvider> {
-            on {
-                getTokens()
-            } doReturn Result.of { throw Exception(throwMessage) }
-        }
-
-        val notaryInit = NotaryInitialization(notaryConfig, passwordConfig, ethWalletProvider, failEthTokenProvider)
-
-        notaryInit.init().fold(
-            { fail { "Exception should be thrown in tokens loader" } },
-            { exc ->
-                assertEquals(throwMessage, exc.message)
-            }
-        )
-    }
 }
