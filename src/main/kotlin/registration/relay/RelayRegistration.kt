1d933b276faa0a66c8194d245d0769e8feaa8317
Alexey <Alexey.N.Chernyshov@gmail.com>
2018-08-06 13:08:35 +0300

    rework RelayRegistration - move iroha network layer away (#102)
    Signed-off-by: Alexey Chernyshov <alexey.n.chernyshov@gmail.com>

--- src/main/kotlin/registration/relay/RelayRegistration.kt
+++ src/main/kotlin/registration/relay/RelayRegistration.kt
@@ -4,19 +4,12 @@ import com.github.kittinunf.result.Result
 import com.github.kittinunf.result.failure
 import com.github.kittinunf.result.map
 import config.EthereumPasswords
-import jp.co.soramitsu.iroha.Keypair
-import jp.co.soramitsu.iroha.ModelTransactionBuilder
 import mu.KLogging
 import notary.EthTokensProvider
 import notary.EthTokensProviderImpl
-import org.web3j.crypto.WalletUtils
-import org.web3j.protocol.Web3j
-import org.web3j.protocol.http.HttpService
 import sidechain.eth.util.DeployHelper
 import sidechain.iroha.consumer.IrohaConsumerImpl
-import sidechain.iroha.consumer.IrohaNetworkImpl
 import sidechain.iroha.util.ModelUtil
-import java.math.BigInteger
 
 /**
  * Class is responsible for relay addresses registration.
@@ -24,7 +17,7 @@ import java.math.BigInteger
  */
 class RelayRegistration(
     val relayRegistrationConfig: RelayRegistrationConfig,
-    val relayRegistrationEthereumPasswords: EthereumPasswords
+    relayRegistrationEthereumPasswords: EthereumPasswords
 ) {
 
     /** Ethereum token list provider */
@@ -61,13 +54,7 @@ class RelayRegistration(
      * @return Result with string representation of hash or possible failure
      */
     private fun sendRelayToIroha(wallet: String): Result<String, Exception> {
-        val utx = ModelTransactionBuilder()
-            .creatorAccountId(creator)
-            .createdTime(ModelUtil.getCurrentTime())
-            .setAccountDetail(notaryIrohaAccount, wallet, "free")
-            .build()
-
-        return irohaConsumer.sendAndCheck(utx)
+        return ModelUtil.setAccountDetail(irohaConsumer, creator, notaryIrohaAccount, wallet, "free")
     }
 
     /**
