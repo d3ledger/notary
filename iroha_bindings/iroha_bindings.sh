b2a2fd89c93cac275bf2dac1d32057e6e7e86f64
Dumitru <dimasavva17@gmail.com>
2018-08-15 16:22:19 +0300

    Feature/remove vendor (#115)
    * Remove vendor
    
    Signed-off-by: Dumitru <dimasavva17@gmail.com>
    
    * Update bindings
    
    Signed-off-by: Dumitru <dimasavva17@gmail.com>
    
    * Add java classes
    
    Signed-off-by: Dumitru <dimasavva17@gmail.com>
    
    * Add binaries
    
    Signed-off-by: Dumitru <dimasavva17@gmail.com>
    
    * Remove binding compilation task and script
    Add todo for downloading
    
    Signed-off-by: Dumitru <dimasavva17@gmail.com>
    
    * Fix gradle
    
    Signed-off-by: Dumitru <dimasavva17@gmail.com>

--- iroha_bindings/iroha_bindings.sh
+++ iroha_bindings/iroha_bindings.sh
@@ -1,27 +1,7 @@
 #!/bin/sh
-# This file generates Iroha bindings based on vendor version of Iroha
+# This file retrieves Iroha bindings from iroha artifact server
 
+#TODO implement artifact downloading
 
-# change dir to script location
-cd "$(dirname "$0")"
-
-build="$1"
-echo "iroha target dir" ${build}
-
-# clear previous bindings
-rm -rf ${build} && mkdir ${build}
-
-bindings_dir=../vendor/iroha/example/java
-
-# generate iroha bindings
-sh ${bindings_dir}/build_library.sh
-
-# move generate files to repository
-cp -R ${bindings_dir}/dist/iroha-bindings-example.jar ${build}/iroha-bindings-example.jar
-cp -R ${bindings_dir}/dist/libirohajava.jnilib ${build}/libirohajava.jnilib
-
-# move protoc library to bindings
-unamestr=`uname`
-if [[ "$unamestr" == 'Linux' ]]; then
-    cp ../vendor/iroha/external/src/google_protobuf-build/libprotobuf.so dist/
-fi
+echo "Not implemented"
+exit 1
