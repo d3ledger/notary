bff5196b5a88d79150edb7452297091c43629ae3
Dumitru <dimasavva17@gmail.com>
2018-08-22 15:37:02 +0300

    Finish integration test
    Signed-off-by: Dumitru <dimasavva17@gmail.com>

--- src/integration-test/kotlin/integration/IrohaBatchTest.kt
+++ src/integration-test/kotlin/integration/IrohaBatchTest.kt
@@ -2,17 +2,23 @@ package integration
 
 import config.loadConfigs
 import jp.co.soramitsu.iroha.ModelCrypto
+import jp.co.soramitsu.iroha.ModelQueryBuilder
 import notary.IrohaCommand
 import notary.IrohaOrderedBatch
 import notary.IrohaTransaction
 import notary.NotaryConfig
 import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.fail
 import sidechain.iroha.consumer.IrohaConsumerImpl
 import sidechain.iroha.consumer.IrohaConverterImpl
+import sidechain.iroha.util.ModelUtil
+import java.math.BigInteger
+import kotlin.test.assertEquals
 
 class IrohaBatchTest {
     @Test
     fun batchTest() {
+        System.loadLibrary("irohajava")
         val tester = "test@notary"
         val notaryConfig = loadConfigs("test", NotaryConfig::class.java)
 
@@ -75,7 +81,92 @@ class IrohaBatchTest {
             )
         )
         val lst = IrohaConverterImpl().convert(batch)
-        val result = irohaConsumer.sendAndCheck(lst)
-        println(result.get())
+        irohaConsumer.sendAndCheck(lst)
+
+
+        val counter = BigInteger.ONE
+        val keypair = ModelUtil.loadKeypair(notaryConfig.iroha.pubkeyPath, notaryConfig.iroha.privkeyPath).get()
+
+        val channel = ModelUtil.getChannel(notaryConfig.iroha.hostname, notaryConfig.iroha.port)
+        val queryStub = ModelUtil.getQueryStub(channel)
+
+        var uquery = ModelQueryBuilder()
+            .creatorAccountId(tester)
+            .queryCounter(counter)
+            .createdTime(ModelUtil.getCurrentTime())
+            .getAccount("u1@notary")
+            .build()
+
+        val account = ModelUtil.prepareQuery(uquery, keypair)
+            .fold(
+                { protoQuery ->
+                    val queryResponse = queryStub.find(protoQuery)
+                    queryResponse.accountResponse.account
+                },
+                {
+                    fail { "Exception while converting byte array to protobuf:" + it.message }
+                }
+            )
+
+        uquery = ModelQueryBuilder()
+            .creatorAccountId(tester)
+            .queryCounter(counter)
+            .createdTime(ModelUtil.getCurrentTime())
+            .getAssetInfo("batches#notary")
+            .build()
+
+        val asset = ModelUtil.prepareQuery(uquery, keypair)
+            .fold(
+                { protoQuery ->
+                    val queryResponse = queryStub.find(protoQuery)
+                    queryResponse.assetResponse.asset
+                },
+                {
+                    fail { "Exception while converting byte array to protobuf:" + it.message }
+                }
+            )
+
+        uquery = ModelQueryBuilder()
+            .creatorAccountId(tester)
+            .queryCounter(counter)
+            .createdTime(ModelUtil.getCurrentTime())
+            .getAccountAssets(tester)
+            .build()
+
+        val tester_amount = ModelUtil.prepareQuery(uquery, keypair)
+            .fold(
+                { protoQuery ->
+                    val queryResponse = queryStub.find(protoQuery)
+                    queryResponse.accountAssetsResponse.accountAssetsList.first().balance
+                },
+                {
+                    fail { "Exception while converting byte array to protobuf:" + it.message }
+                }
+            )
+
+
+        uquery = ModelQueryBuilder()
+            .creatorAccountId(tester)
+            .queryCounter(counter)
+            .createdTime(ModelUtil.getCurrentTime())
+            .getAccountAssets("u1@notary")
+            .build()
+
+        val u1_amount = ModelUtil.prepareQuery(uquery, keypair)
+            .fold(
+                { protoQuery ->
+                    val queryResponse = queryStub.find(protoQuery)
+                    queryResponse.accountAssetsResponse.accountAssetsList.first().balance
+                },
+                {
+                    fail { "Exception while converting byte array to protobuf:" + it.message }
+                }
+            )
+
+        assertEquals(account.accountId, "u1@notary")
+        assertEquals(account.jsonData, "{\"test@notary\": {\"key\": \"value\"}}")
+        assertEquals(asset.assetId, "batches#notary")
+        assertEquals(tester_amount.toInt(), 73)
+        assertEquals(u1_amount.toInt(), 27)
     }
 }
