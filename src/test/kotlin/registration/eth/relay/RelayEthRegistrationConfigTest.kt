fb60491b0d15a224dcb57fe29f367db8971b76d1
Alexey Rodionov <rodionov12@gmail.com>
2018-09-14 09:37:28 +0300

    hostname
    Signed-off-by: Alexey Rodionov <rodionov12@gmail.com>

--- src/test/kotlin/registration/eth/relay/RelayEthRegistrationConfigTest.kt
+++ src/test/kotlin/registration/eth/relay/RelayEthRegistrationConfigTest.kt
@@ -20,7 +20,7 @@ class RelayEthRegistrationConfigTest {
         assertEquals("notary_red@notary", relayRegistrationConfig.notaryIrohaAccount)
         assertEquals("0x57dd50c6b4ec7a33ee3bb5056bf38856e181684c", relayRegistrationConfig.ethMasterWallet)
 
-        assertEquals("localhost", relayRegistrationConfig.iroha.hostname)
+        assertEquals("d3-iroha", relayRegistrationConfig.iroha.hostname)
         assertEquals(50051, relayRegistrationConfig.iroha.port)
         assertEquals("eth_registration_service_red@notary", relayRegistrationConfig.iroha.creator)
         assertEquals("deploy/iroha/keys/admin@notary.pub", relayRegistrationConfig.iroha.pubkeyPath)
