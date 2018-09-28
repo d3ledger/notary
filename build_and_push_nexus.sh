d28c7e3b889ff46e9d63baecff319b9c41afbf4d
Dumitru <dimasavva17@gmail.com>
2018-09-28 10:02:27 +0300

    Check profile in build and push script
    Signed-off-by: Dumitru <dimasavva17@gmail.com>

--- build_and_push_nexus.sh
+++ build_and_push_nexus.sh
@@ -1,15 +1,31 @@
 #!/usr/bin/env bash
+set -e
+
+PROFILE=$1
+
+checkProfile () {
+  local e
+  for e in "${@:2}"; do [[ "$e" == "$1" ]] && return 0; done
+  echo "Invalid profile"
+  echo "Usage: ./build_and_push_nexus.sh <local | testnet | mainnet>"
+  exit 1
+}
+
+profiles=("local" "testnet" "mainnet")
+
+checkProfile $PROFILE "${profiles[@]}"
+
 
 rm build/libs/notary-1.0-SNAPSHOT-all.jar || true
-gradle shadowJar -Pprofile=testnet
+gradle shadowJar -Pprofile=$PROFILE
 
-docker build -t nexus.iroha.tech:19002/d3-deploy/eth-relay:testnet -f eth-relay.dockerfile .
-docker build -t nexus.iroha.tech:19002/d3-deploy/registration:testnet  -f registration.dockerfile .
-docker build -t nexus.iroha.tech:19002/d3-deploy/notary:testnet  -f notary.dockerfile .
-docker build -t nexus.iroha.tech:19002/d3-deploy/withdrawal:testnet  -f withdrawal.dockerfile .
+docker build -t nexus.iroha.tech:19002/d3-deploy/eth-relay:$PROFILE -f eth-relay.dockerfile .
+docker build -t nexus.iroha.tech:19002/d3-deploy/registration:$PROFILE  -f registration.dockerfile .
+docker build -t nexus.iroha.tech:19002/d3-deploy/notary:$PROFILE  -f notary.dockerfile .
+docker build -t nexus.iroha.tech:19002/d3-deploy/withdrawal:$PROFILE  -f withdrawal.dockerfile .
 
 
-docker push nexus.iroha.tech:19002/d3-deploy/eth-relay:testnet
-docker push nexus.iroha.tech:19002/d3-deploy/registration:testnet
-docker push nexus.iroha.tech:19002/d3-deploy/notary:testnet
-docker push nexus.iroha.tech:19002/d3-deploy/withdrawal:testnet
+docker push nexus.iroha.tech:19002/d3-deploy/eth-relay:$PROFILE
+docker push nexus.iroha.tech:19002/d3-deploy/registration:$PROFILE
+docker push nexus.iroha.tech:19002/d3-deploy/notary:$PROFILE
+docker push nexus.iroha.tech:19002/d3-deploy/withdrawal:$PROFILE
