fccad74e72787e1a2e5cb5d8ee3ab084101a5496
Alexey Rodionov <rodionov12@gmail.com>
2018-09-29 15:04:35 +0300

    BTC tests update
    Signed-off-by: Alexey Rodionov <rodionov12@gmail.com>

--- notary-integration-test/src/integration-test/kotlin/integration/TestConfig.kt
+++ notary-integration-test/src/integration-test/kotlin/integration/TestConfig.kt
@@ -1,14 +1,7 @@
 package integration
 
-import config.EthereumConfig
-import config.IrohaConfig
-
 interface TestConfig {
     val notaryIrohaAccount: String
     val tokenStorageAccount: String
-    val registrationIrohaAccount: String
     val whitelistSetter: String
-    val ethTestAccount: String
-    val ethereum: EthereumConfig
-    val iroha: IrohaConfig
 }
