f0c28fd16d50eabb25771ac7bc9ee5984df1b2ad
Alexey Rodionov <rodionov12@gmail.com>
2018-09-29 15:11:02 +0300

    Reset tests
    Signed-off-by: Alexey Rodionov <rodionov12@gmail.com>

---
+++ notary-integration-test/src/integration-test/kotlin/integration/eth/FailedTransactionTest.kt
@@ -0,0 +1,74 @@
+package integration.eth
+
+import integration.helper.IntegrationHelperUtil
+import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.TestInstance
+import org.web3j.protocol.exceptions.TransactionException
+import java.math.BigInteger
+import util.getRandomString
+import java.math.BigDecimal
+import kotlin.test.assertEquals
+
+@TestInstance(TestInstance.Lifecycle.PER_CLASS)
+class FailedTransactionTest {
+    val integrationHelper = IntegrationHelperUtil()
+
+    init {
+        integrationHelper.runEthNotary()
+    }
+
+    /**
+     * Reverted Ether transfer transaction test
+     * @given Ethereum node, Iroha and notary are running, failer contract is deployed and registered as relay,
+     * new user registers in Iroha
+     * @when send Ether to relay account
+     * @then user account in Iroha has 0 balance of Ether
+     */
+    @Test
+    fun failedEtherTransferTest() {
+        val failerAddress = integrationHelper.deployFailer()
+        integrationHelper.registerRelayByAddress(failerAddress)
+        val clientAccount = String.getRandomString(9)
+        integrationHelper.registerClientWithoutRelay(clientAccount)
+        integrationHelper.sendEth(BigInteger.valueOf(1), failerAddress)
+        integrationHelper.waitOneEtherBlock()
+        assertEquals(BigInteger.ZERO, integrationHelper.getEthBalance(failerAddress))
+        val irohaBalance = integrationHelper.getIrohaAccountBalance("$clientAccount@notary", "ether#ethereum")
+        assertEquals(BigDecimal.ZERO, BigDecimal(irohaBalance))
+    }
+
+    /**
+     * Reverted Ether transfer transaction test
+     * @given Ethereum node, Iroha and notary are running, failer contract is deployed twice,
+     * one of them is registered as relay, another one is registered as ERC-20 token and
+     * new user registers in Iroha
+     * @when send tokens to relay account
+     * @then user account in Iroha has 0 balance of ERC-20 token
+     */
+    @Test
+    fun failedTokenTransferTest() {
+        val failerAddress = integrationHelper.deployFailer()
+        val anotherFailerAddress = integrationHelper.deployFailer()
+        integrationHelper.registerRelayByAddress(failerAddress)
+        val clientAccount = String.getRandomString(9)
+        integrationHelper.registerClientWithoutRelay(clientAccount)
+        val coinName = String.getRandomString(9)
+        integrationHelper.addERC20Token(anotherFailerAddress, coinName, 0)
+
+        // web3j throws exception in case of contract function call revert
+        // so let's catch and ignore it
+        try {
+            integrationHelper.sendERC20Token(anotherFailerAddress, BigInteger.valueOf(1), failerAddress)
+        } catch (e: TransactionException) {
+        }
+
+        integrationHelper.waitOneEtherBlock()
+
+        // actually this test passes even without transaction status check
+        // it's probably impossible to get some money deposit to iroha
+        // because logs are empty for reverted transactions
+        // but let's leave it for a rainy day
+        val irohaBalance = integrationHelper.getIrohaAccountBalance("$clientAccount@notary", "$coinName#ethereum")
+        assertEquals(BigDecimal.ZERO, BigDecimal(irohaBalance))
+    }
+}
---
+++ notary-integration-test/src/integration-test/kotlin/integration/iroha/IrohaBatchTest.kt
@@ -0,0 +1,263 @@
+package integration.iroha
+
+import config.loadConfigs
+import jp.co.soramitsu.iroha.Blob
+import jp.co.soramitsu.iroha.ModelCrypto
+import jp.co.soramitsu.iroha.iroha
+import kotlinx.coroutines.experimental.async
+import kotlinx.coroutines.experimental.runBlocking
+import kotlinx.coroutines.experimental.withTimeout
+import mu.KLogging
+import notary.IrohaCommand
+import notary.IrohaOrderedBatch
+import notary.IrohaTransaction
+import notary.eth.EthNotaryConfig
+import org.junit.jupiter.api.Test
+import sidechain.iroha.IrohaChainListener
+import sidechain.iroha.consumer.IrohaConsumerImpl
+import sidechain.iroha.consumer.IrohaConverterImpl
+import sidechain.iroha.consumer.IrohaNetworkImpl
+import sidechain.iroha.util.ModelUtil
+import sidechain.iroha.util.getAccountAsset
+import sidechain.iroha.util.getAccountData
+import sidechain.iroha.util.toByteVector
+import util.getRandomString
+import java.util.concurrent.TimeUnit
+import kotlin.test.assertEquals
+
+class IrohaBatchTest {
+
+    init {
+        System.loadLibrary("irohajava")
+    }
+
+    val testConfig by lazy {
+        loadConfigs("test", EthNotaryConfig::class.java)
+    }
+
+    private val tester = "test@notary"
+
+    private val keypair by lazy {
+        ModelUtil.loadKeypair(testConfig.iroha.pubkeyPath, testConfig.iroha.privkeyPath).get()
+    }
+
+    private val irohaNetwork = IrohaNetworkImpl(testConfig.iroha.hostname, testConfig.iroha.port)
+
+    private fun randomString() = String.getRandomString(10)
+
+    /**
+     * @given A batch
+     * @when All transactions are valid and sent as a batch
+     * @then They all are accepted and committed in the same block
+     */
+    @Test
+    fun allValidBatchTest() {
+
+        val user = randomString()
+        val asset_name = randomString()
+
+        val irohaConsumer = IrohaConsumerImpl(testConfig.iroha.creator, testConfig.iroha)
+
+        val txList =
+            listOf(
+                IrohaTransaction(
+                    tester,
+                    ModelUtil.getCurrentTime(),
+                    1,
+                    listOf(
+                        IrohaCommand.CommandCreateAccount(
+                            user,
+                            "notary",
+                            ModelCrypto().generateKeypair().publicKey().hex()
+                        )
+                    )
+                ),
+                IrohaTransaction(
+                    tester,
+                    ModelUtil.getCurrentTime(),
+                    1,
+                    listOf(
+                        IrohaCommand.CommandSetAccountDetail(
+                            "$user@notary",
+                            "key",
+                            "value"
+                        )
+                    )
+                ),
+                IrohaTransaction(
+                    tester,
+                    ModelUtil.getCurrentTime(),
+                    1,
+                    listOf(
+                        IrohaCommand.CommandCreateAsset(
+                            asset_name,
+                            "notary",
+                            0
+                        ),
+                        IrohaCommand.CommandAddAssetQuantity(
+                            "$asset_name#notary",
+                            "100"
+                        ),
+                        IrohaCommand.CommandTransferAsset(
+                            tester,
+                            "$user@notary",
+                            "$asset_name#notary",
+                            "desc",
+                            "27"
+                        )
+                    )
+                )
+
+            )
+
+        val batch = IrohaOrderedBatch(txList)
+        val lst = IrohaConverterImpl().convert(batch)
+        val hashes = lst.map { it.hash().hex() }
+
+        val listener = IrohaChainListener(
+            testConfig.iroha.hostname,
+            testConfig.iroha.port,
+            tester, keypair
+        )
+
+        val blockHashes = async {
+            listener.getBlock().payload.transactionsList.map {
+                Blob(iroha.hashTransaction(it.toByteArray().toByteVector())).hex()
+            }
+        }
+
+        val successHash = irohaConsumer.sendAndCheck(lst).get()
+
+        val accountJson = getAccountData(testConfig.iroha, keypair, irohaNetwork, "$user@notary").get().toJsonString()
+        val tester_amount = getAccountAsset(testConfig.iroha, keypair, irohaNetwork, tester, "$asset_name#notary").get()
+        val u1_amount =
+            getAccountAsset(testConfig.iroha, keypair, irohaNetwork, "$user@notary", "$asset_name#notary").get()
+
+        assertEquals(hashes, successHash)
+        assertEquals("{\"test@notary\":{\"key\":\"value\"}}", accountJson)
+        assertEquals(73, tester_amount.toInt())
+        assertEquals(27, u1_amount.toInt())
+
+        runBlocking {
+            withTimeout(10, TimeUnit.SECONDS) {
+                assertEquals(hashes, blockHashes.await())
+            }
+        }
+    }
+
+    /**
+     * @given A batch
+     * @when Not all transactions are valid and sent as a batch
+     * @then Only valid are accepted and committed in the same block
+     */
+    @Test
+    fun notAllValidBatchTest() {
+        val user = randomString()
+        val asset_name = randomString()
+
+        val irohaConsumer = IrohaConsumerImpl(testConfig.iroha.creator, testConfig.iroha)
+
+        val txList =
+            listOf(
+                IrohaTransaction(
+                    tester,
+                    ModelUtil.getCurrentTime(),
+                    1,
+                    listOf(
+                        IrohaCommand.CommandCreateAccount(
+                            user,
+                            "notary",
+                            ModelCrypto().generateKeypair().publicKey().hex()
+                        )
+                    )
+                ),
+                IrohaTransaction(
+                    tester,
+                    ModelUtil.getCurrentTime(),
+                    1,
+                    listOf(
+                        IrohaCommand.CommandSetAccountDetail(
+                            "$user@notary",
+                            "key",
+                            "value"
+                        )
+                    )
+                ),
+                IrohaTransaction(
+                    tester,
+                    ModelUtil.getCurrentTime(),
+                    1,
+                    listOf(
+                        IrohaCommand.CommandCreateAsset(
+                            asset_name,
+                            "notary",
+                            0
+                        ),
+                        IrohaCommand.CommandAddAssetQuantity(
+                            "$asset_name#notary",
+                            "100"
+                        ),
+                        IrohaCommand.CommandTransferAsset(
+                            tester,
+                            "$user@notary",
+                            "$asset_name#notary",
+                            "desc",
+                            "27"
+                        )
+                    )
+                ),
+                IrohaTransaction(
+                    tester,
+                    ModelUtil.getCurrentTime(),
+                    1,
+                    listOf(
+                        IrohaCommand.CommandTransferAsset(
+                            tester,
+                            "$user@notary",
+                            "$asset_name#notary",
+                            "",
+                            "1234"
+                        )
+                    )
+                )
+
+            )
+
+        val batch = IrohaOrderedBatch(txList)
+        val lst = IrohaConverterImpl().convert(batch)
+        val hashes = lst.map { it.hash().hex() }
+        val expectedHashes = hashes.subList(0, hashes.size - 1)
+
+        val listener = IrohaChainListener(
+            testConfig.iroha.hostname,
+            testConfig.iroha.port,
+            tester,
+            keypair
+        )
+
+        val blockHashes = async {
+            listener.getBlock().payload.transactionsList.map {
+                Blob(iroha.hashTransaction(it.toByteArray().toByteVector())).hex()
+            }
+        }
+
+        val successHash = irohaConsumer.sendAndCheck(lst).get()
+
+        val accountJson = getAccountData(testConfig.iroha, keypair, irohaNetwork, "$user@notary").get().toJsonString()
+        val tester_amount = getAccountAsset(testConfig.iroha, keypair, irohaNetwork, tester, "$asset_name#notary").get()
+        val u1_amount =
+            getAccountAsset(testConfig.iroha, keypair, irohaNetwork, "$user@notary", "$asset_name#notary").get()
+
+        assertEquals(expectedHashes, successHash)
+        assertEquals("{\"test@notary\":{\"key\":\"value\"}}", accountJson)
+        assertEquals(73, tester_amount.toInt())
+        assertEquals(27, u1_amount.toInt())
+
+        runBlocking {
+            withTimeout(10, TimeUnit.SECONDS) {
+                assertEquals(expectedHashes, blockHashes.await())
+            }
+        }
+    }
+}
+
