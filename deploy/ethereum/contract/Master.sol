cb2a6b0fa66c2ef3cc635ebf7284d274cf204887
Andrey Zolin <crrrr@mail.ru>
2018-10-01 11:47:53 +0300

    fix naming

--- deploy/ethereum/contract/Master.sol
+++ deploy/ethereum/contract/Master.sol
@@ -103,7 +103,7 @@ contract Master {
      * @param v array of signatures of tx_hash (v-component)
      * @param r array of signatures of tx_hash (r-component)
      * @param s array of signatures of tx_hash (s-component)
-     * @param _from relay contract address
+     * @param from relay contract address
      */
     function withdraw(
         address tokenAddress,
