9b67510f9347adb7b93490f33fef0735ec64a182
Andrey Zolin <giorkin@gmail.com>
2018-10-01 11:51:47 +0300

    Smart contracts/codestyle (#194)
    * Smart contracts update code style according to https://solidity.readthedocs.io/en/v0.4.25/style-guide.html

---
+++ deploy/ethereum/contract/IMaster.sol
@@ -0,0 +1,19 @@
+pragma solidity 0.4.25;
+
+/**
+ * Subset of master contract interface
+ */
+contract IMaster {
+    function withdraw(
+        address token_address,
+        uint256 amount,
+        address to,
+        bytes32 tx_hash,
+        uint8[] v,
+        bytes32[] r,
+        bytes32[] s,
+        address from
+    )
+    public;
+    function checkTokenAddress(address token) public view returns (bool);
+}
