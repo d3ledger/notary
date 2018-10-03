8ed37c1902c7fac56c57ce315b195a99ecb4ed16
Dumitru <dimasavva17@gmail.com>
2018-10-04 00:13:31 +0300

    push
    Signed-off-by: Dumitru <dimasavva17@gmail.com>

--- build_and_push_nexus.sh
+++ build_and_push_nexus.sh
@@ -7,11 +7,11 @@ checkTag () {
   local e
   for e in "${@:2}"; do [[ "$e" == "$1" ]] && return 0; done
   echo "Invalid tag"
-  echo "Usage: ./build_and_push_nexus.sh <master | develop | debug>"
+  echo "Usage: ./build_and_push_nexus.sh <master | develop | debug | reserved> "
   exit 1
 }
 
-tags=("master" "develop" "debug")
+tags=("master" "develop" "debug", "reserved")
 
 checkTag $TAG "${tags[@]}"
 
