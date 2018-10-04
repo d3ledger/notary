45fed78790ea42da65f8a773be6be7e886c5d86e
Alexey Chernyshov <alexey.n.chernyshov@gmail.com>
2018-10-04 17:10:11 +0300

    add notary keys
    Signed-off-by: Alexey Chernyshov <alexey.n.chernyshov@gmail.com>

--- test-longevity/src/main/kotlin/main.kt
+++ test-longevity/src/main/kotlin/main.kt
@@ -4,6 +4,7 @@ import config.IrohaCredentialConfig
 import integration.helper.IntegrationHelperUtil
 import jp.co.soramitsu.iroha.ModelCrypto
 import model.IrohaCredential
+import java.io.File
 import javax.jws.WebParam
 
 /** Path to public key of 2nd instance of notary */
@@ -18,37 +19,47 @@ private val privkeyPath2 = "deploy/iroha/keys/notary2@notary.priv"
 fun main(args: Array<String>) {
     val integrationHelper = IntegrationHelperUtil()
 
+//
+//    ///////
+//
+//    val notaryConfig = integrationHelper.configHelper.createEthNotaryConfig()
+//
+//    val keypair = ModelCrypto().generateKeypair()
+//    val credential = IrohaCredential(integrationHelper.accountHelper.notaryAccount.accountId, keypair)
+//
+//    ///////
+//
+//
+//    // create 2nd notray config
+//    val irohaConfig =
+//        integrationHelper.configHelper.createIrohaConfig()
+//
+//    val notaryCredential2 = object : IrohaCredentialConfig {
+//        override val pubkeyPath: String
+//            get() = pubkeyPath2
+//        override val privkeyPath: String
+//            get() = privkeyPath2
+//        override val accountId: String
+//            get() = integrationHelper.accountHelper.notaryAccount.accountId
+//    }
+//
+////    val notaryConfig = integrationHelper.configHelper.createEthNotaryConfig(
+////        irohaConfig,
+////        integrationHelper.configHelper.ethNotaryConfig.ethereum,
+////        notaryCredential2
+////    )
+//
+//
+//    // run 2nd instance of notary
+//    integrationHelper.runEthNotary(notaryConfig)
 
-    ///////
+    val keys = ModelCrypto().generateKeypair()
 
-    val notaryConfig = integrationHelper.configHelper.createEthNotaryConfig()
-
-    val keypair = ModelCrypto().generateKeypair()
-    val credential = IrohaCredential(integrationHelper.accountHelper.notaryAccount.accountId, keypair)
-
-    ///////
-
-
-    // create 2nd notray config
-    val irohaConfig =
-        integrationHelper.configHelper.createIrohaConfig()
-
-    val notaryCredential2 = object : IrohaCredentialConfig {
-        override val pubkeyPath: String
-            get() = pubkeyPath2
-        override val privkeyPath: String
-            get() = privkeyPath2
-        override val accountId: String
-            get() = integrationHelper.accountHelper.notaryAccount.accountId
+    File("notary1@notary.pub").printWriter().use { out ->
+        out.print(keys.publicKey().hex())
     }
 
-//    val notaryConfig = integrationHelper.configHelper.createEthNotaryConfig(
-//        irohaConfig,
-//        integrationHelper.configHelper.ethNotaryConfig.ethereum,
-//        notaryCredential2
-//    )
-
-
-    // run 2nd instance of notary
-    integrationHelper.runEthNotary(notaryConfig)
+    File("notary1@notar.priv").printWriter().use { out ->
+        out.print(keys.privateKey().hex())
+    }
 }
