724991ccf71b61735de6d301e11febbd7179b169
Dumitru <dimasavva17@gmail.com>
2018-10-10 14:18:11 +0300

    Wait for iroha to start
    Signed-off-by: Dumitru <dimasavva17@gmail.com>

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-if [ -z "${IROHA_NETWORK}" ] || [ -z "${IROHA_HOST}" ] || [ -z "${IROHA_PORT}" ] || [ -z "${CLASS}" ]; then
+if  [ -z "${IROHA_HOST}" ] || [ -z "${IROHA_PORT}" ] || [ -z "${CLASS}" ]; then
     echo "Please set IROHA_NETWORK, IROHA_HOST, IROHA_PORT and CLASS variables"
     exit
 fi
@@ -8,7 +8,7 @@ fi
 STATE=0
 
 while [ $STATE -ne 1 ]; do
-    let STATE="$(docker run --network="${IROHA_NETWORK}" x3medima17/grpc_healthcheck ${IROHA_HOST} ${IROHA_PORT})"
+    let STATE="$(./grpc_healthcheck ${IROHA_HOST} ${IROHA_PORT})"
     echo "Waiting for iroha..."
     sleep 0.1
 done
--- docker/eth-relay.dockerfile
+++ docker/eth-relay.dockerfile
@@ -6,5 +6,9 @@ COPY build/libs/notary-1.0-SNAPSHOT-all.jar /opt/notary/notary.jar
 COPY iroha_bindings/linux/* /opt/notary/
 ENV LD_LIBRARY_PATH="/opt/notary:${LD_LIBRARY_PATH}"
 
+# Please run `kscript --package grpc_healthcheck.kts` before building docker image
+COPY grpc_healthcheck /opt/notary
+
 COPY docker/entrypoint.sh /opt/notary/
+
 ENTRYPOINT ["/opt/notary/entrypoint.sh"]
