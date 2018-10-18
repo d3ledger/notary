eff8c3950b59279efe940dc17c0b7e0a7e5fc639
Alexey Rodionov <rodionov12@gmail.com>
2018-10-18 13:12:24 +0300

    Fix hostname
    Signed-off-by: Alexey Rodionov <rodionov12@gmail.com>

--- notary-integration-test/src/main/kotlin/integration/helper/IntegrationHelperUtil.kt
+++ notary-integration-test/src/main/kotlin/integration/helper/IntegrationHelperUtil.kt
@@ -135,7 +135,7 @@ class IntegrationHelperUtil {
         BitcoinRpcClientFactory.createClient(
             user = "test",
             password = "test",
-            host = "localhost",//configHelper.createBtcNotaryConfig().bitcoin.host,
+            host = "d3-btc-node0",//configHelper.createBtcNotaryConfig().bitcoin.host,
             port = 8332,
             secure = false
         )
