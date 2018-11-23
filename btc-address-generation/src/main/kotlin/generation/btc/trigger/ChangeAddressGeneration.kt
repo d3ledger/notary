5e7f30a516d56fb060add18179bab535c81d7c71
dolgopolovwork <37849469+dolgopolovwork@users.noreply.github.com>
2018-11-23 17:31:41 +0300

    Address generation was moved to Spring (#282)

--- btc-address-generation/src/main/kotlin/generation/btc/trigger/ChangeAddressGeneration.kt
+++ btc-address-generation/src/main/kotlin/generation/btc/trigger/ChangeAddressGeneration.kt
@@ -2,10 +2,32 @@
 
 package generation.btc.trigger
 
-import generation.btc.config.btcAddressGenerationConfig
+import com.github.kittinunf.result.flatMap
+import com.github.kittinunf.result.map
+import mu.KLogging
+import org.springframework.context.annotation.AnnotationConfigApplicationContext
+import org.springframework.context.annotation.ComponentScan
 import provider.btc.address.BtcAddressType
+import sidechain.iroha.IrohaInitialization
 
-// Generate Bitcoin change address
+@ComponentScan(basePackages = ["generation.btc.trigger"])
+class BtcChangeAddressGenerationApplication
+
+private val logger = KLogging().logger
+/*
+   Change address generation entry point
+ */
 fun main(args: Array<String>) {
-    startAddressGeneration(btcAddressGenerationConfig, BtcAddressType.CHANGE)
+    val addressType = BtcAddressType.CHANGE
+    IrohaInitialization.loadIrohaLibrary().map {
+        AnnotationConfigApplicationContext(BtcChangeAddressGenerationApplication::class.java)
+    }.flatMap { context ->
+        context.getBean(AddressGenerationTrigger::class.java).startAddressGeneration(addressType)
+    }.fold(
+        {
+            logger.info { "${addressType.title} address generation process was started" }
+        }, { ex ->
+            logger.error("Cannot start ${addressType.title} address generation process", ex)
+            System.exit(1)
+        })
 }
