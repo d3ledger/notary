9736629c63fbce57523ff5aea0f12c2e3a3cf32f
Anton Dolgopolov <dolgopolov@soramitsu.co.jp>
2018-11-23 15:38:57 +0300

    Address generation was moved to Spring

--- btc-address-generation/src/main/kotlin/generation/btc/trigger/FreeAddressGeneration.kt
+++ btc-address-generation/src/main/kotlin/generation/btc/trigger/FreeAddressGeneration.kt
@@ -2,10 +2,32 @@
 
 package generation.btc.trigger
 
-import generation.btc.config.btcAddressGenerationConfig
+import com.github.kittinunf.result.flatMap
+import com.github.kittinunf.result.map
+import mu.KLogging
+import org.springframework.context.annotation.AnnotationConfigApplicationContext
+import org.springframework.context.annotation.ComponentScan
 import provider.btc.address.BtcAddressType
+import sidechain.iroha.IrohaInitialization
 
-// Generates Bitcoin free address that can be registered later
+@ComponentScan(basePackages = ["generation.btc.trigger"])
+class BtcFreeAddressGenerationApplication
+
+private val logger = KLogging().logger
+/*
+   Free address generation entry point
+ */
 fun main(args: Array<String>) {
-    startAddressGeneration(btcAddressGenerationConfig, BtcAddressType.FREE)
+    val addressType = BtcAddressType.FREE
+    IrohaInitialization.loadIrohaLibrary().map {
+        AnnotationConfigApplicationContext(BtcFreeAddressGenerationApplication::class.java)
+    }.flatMap { context ->
+        context.getBean(AddressGenerationTrigger::class.java).startAddressGeneration(addressType)
+    }.fold(
+        {
+            logger.info { "${addressType.title} address generation process was started" }
+        }, { ex ->
+            logger.error("Cannot start ${addressType.title} address generation process", ex)
+            System.exit(1)
+        })
 }
