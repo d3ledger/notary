2ce2d1ffcdb09ced178ec39fb01e3baeb2977871
Alexey Chernyshov <alexey.n.chernyshov@gmail.com>
2019-03-12 10:55:13 +0300

    fix package name in registration service
    Signed-off-by: Alexey Chernyshov <alexey.n.chernyshov@gmail.com>

--- notary-registration/src/main/kotlin/com/d3/commons/registration/main.kt
+++ notary-registration/src/main/kotlin/com/d3/commons/registration/main.kt
@@ -12,7 +12,7 @@ import org.springframework.context.annotation.ComponentScan
 
 private val logger = KLogging().logger
 
-@ComponentScan(basePackages = ["registration"])
+@ComponentScan(basePackages = ["com.d3.commons.registration"])
 class RegistrationApplication
 
 /**
