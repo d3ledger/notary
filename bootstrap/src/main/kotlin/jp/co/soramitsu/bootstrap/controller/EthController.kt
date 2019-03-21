6cdd2bb0f4999f8755f6617cd2219434cca3f08d
Iurii <discours@list.ru>
2019-03-21 15:19:02 +0300

    - github #434 Bootstrap service should have an endpoint for listing Ethereum wallets required for other services

--- bootstrap/src/main/kotlin/jp/co/soramitsu/bootstrap/controller/EthController.kt
+++ bootstrap/src/main/kotlin/jp/co/soramitsu/bootstrap/controller/EthController.kt
@@ -5,6 +5,7 @@ import mu.KLogging
 import org.springframework.http.HttpStatus
 import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.PathVariable
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
 import org.web3j.crypto.Keys
@@ -28,5 +29,19 @@ class EthController {
             response.message = e.message
             return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(response)
         }
+    }
+
+    @GetMapping("/list/servicesWithWallet/{peersCount}")
+    fun listServicesWithEthWallet(@PathVariable("peersCount")peersCount: Int): ResponseEntity<List<String>> {
+        val list = ArrayList<String>()
+        var depositCounter = peersCount
+        while(depositCounter > 0) {
+            list.add("eth-deposit-service-peer$peersCount")
+            depositCounter--
+        }
+        list.add("eth-registration-service")
+        list.add("eth-withdrawal-service")
+        list.add("eth-genesis-wallet")
+        return ResponseEntity.ok<List<String>>(list)
     }
 }
