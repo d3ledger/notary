f08e5b71663568a9ee99eafa9cb1ca4e6d3a8373
Iury V. Vinogradov <discours@list.ru>
2019-03-28 16:34:23 +0300

    Github #460 #453 Roles with conflicting name, Adjust Ethereum wallet endpoint (#461)
    * - Generate genesis block API Check for empty peer keys
    
    * - #426 Quorum returned by /iroha/config/accounts/{project}/{env} endpoint does not consider number of Iroha peers
    
    * - #426 Quorum returned by /iroha/config/accounts/{project}/{env} endpoint does not consider number of Iroha peers
    
    * - #426 fix GenesisBlock syntax
    
    * - #426 update test
    
    * - #426 update test
    
    * - #430 pull request adjustments
    
    * - github #427 Bootstrap service `setAccountDetail` blocks
    
    * - github #441 BTC_NETWORK environment variable
    
    * - github #441 BTC_NETWORK environment variable
    
    * - github #434 Bootstrap service should have an endpoint for listing Ethereum wallets required for other services
    
    * - github #434 Bootstrap service should have an endpoint for listing Ethereum wallets required for other services
    
    * - github #453 Bootstrap service creates roles with conflicting names in a Genesis Block
    - github #454 Missing role definition in Genesis Block produced by Bootstrap service
    
    * - adjust test
    
    * -  Github #460 Bootstrap service: Adjust Ethereum wallet endpoint
    
    * -  Adjust list service eth wallets
    
    * - Github #433 Master contract deployment with Bootstrap service

--- bootstrap/src/main/kotlin/jp/co/soramitsu/bootstrap/controller/EthController.kt
+++ bootstrap/src/main/kotlin/jp/co/soramitsu/bootstrap/controller/EthController.kt
@@ -1,29 +1,65 @@
 package jp.co.soramitsu.bootstrap.controller
 
+import com.d3.eth.sidechain.util.DeployHelperBuilder
 import jp.co.soramitsu.bootstrap.dto.EthWallet
+import jp.co.soramitsu.bootstrap.dto.MasterContractResponse
+import jp.co.soramitsu.bootstrap.dto.MasterContractsRequest
 import mu.KLogging
 import org.springframework.http.HttpStatus
 import org.springframework.http.ResponseEntity
-import org.springframework.web.bind.annotation.GetMapping
-import org.springframework.web.bind.annotation.PathVariable
-import org.springframework.web.bind.annotation.RequestMapping
-import org.springframework.web.bind.annotation.RestController
+import org.springframework.web.bind.annotation.*
 import org.web3j.crypto.Keys
 import org.web3j.crypto.Wallet
 import org.web3j.crypto.WalletFile
+import javax.validation.constraints.NotNull
 
 @RestController
 @RequestMapping("/eth")
 class EthController {
     private val log = KLogging().logger
 
+    @PostMapping("/deploy/D3/smartContracts")
+    fun deployMasterSmartContracts(@NotNull @RequestBody request: MasterContractsRequest): ResponseEntity<MasterContractResponse> {
+
+        log.info { "Run predeploy with notary addresses: ${request.notaryEthereumAccounts}" }
+        var response: MasterContractResponse
+        return if (request.network != null) {
+            try {
+                val deployHelper = DeployHelperBuilder(
+                    request.network.ethereumConfig,
+                    request.network.ethPasswords
+                ).setFastTransactionManager()
+                    .build()
+                val relayRegistry = deployHelper.deployUpgradableRelayRegistrySmartContract()
+                val master =
+                    deployHelper.deployUpgradableMasterSmartContract(
+                        relayRegistry.contractAddress,
+                        request.notaryEthereumAccounts
+                    )
+                val relayImplementation = deployHelper.deployRelaySmartContract(master.contractAddress)
+                response = MasterContractResponse(
+                    master.contractAddress,
+                    relayRegistry.contractAddress,
+                    relayImplementation.contractAddress,
+                    master.tokens.send()[0].toString()
+                )
+            } catch(e:Exception) {
+                log.error("Cannot deploy smart contract", e)
+                response = MasterContractResponse(e.javaClass.simpleName, e.message)
+            }
+            ResponseEntity.status(HttpStatus.CONFLICT).body(response)
+        } else {
+            ResponseEntity.status(HttpStatus.BAD_REQUEST).body(MasterContractResponse(HttpStatus.BAD_REQUEST.name, "Network parameters are not set"))
+        }
+    }
+
     @GetMapping("/create/wallet")
-    fun createWallet(): ResponseEntity<EthWallet> {
+    fun createWallet(@NotNull @RequestParam password: String): ResponseEntity<EthWallet> {
         try {
-            val wallet: WalletFile = Wallet.createStandard("AB", Keys.createEcKeyPair())
+            val wallet: WalletFile = Wallet.createStandard(password, Keys.createEcKeyPair())
             return ResponseEntity.ok<EthWallet>(EthWallet(wallet))
-        } catch(e:Exception) {
-            log.error("Error creating Ethereum wallet",e)
+        } catch (e: Exception) {
+            log.error("Error creating Ethereum wallet", e)
             val response = EthWallet()
             response.errorCode = e.javaClass.simpleName
             response.message = e.message
@@ -32,11 +68,11 @@ class EthController {
     }
 
     @GetMapping("/list/servicesWithWallet/d3/{peersCount}")
-    fun listServicesWithEthWallet(@PathVariable("peersCount")peersCount: Int): ResponseEntity<List<String>> {
+    fun listServiceEthWallets(@PathVariable("peersCount") peersCount: Int): ResponseEntity<List<String>> {
         val list = ArrayList<String>()
         var depositCounter = peersCount
-        while(depositCounter > 0) {
-            list.add("eth-deposit-service-peer$peersCount")
+        while (depositCounter > 0) {
+            list.add("eth-deposit-service-peer$depositCounter")
             depositCounter--
         }
         list.add("eth-registration-service")
