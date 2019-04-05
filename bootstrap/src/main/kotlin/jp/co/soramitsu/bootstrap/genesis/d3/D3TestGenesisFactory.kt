b8dc894fa7274532581021fb3c319cc1231130b9
iurii.vinogradov <discours@list.ru>
2019-04-05 13:58:33 +0300

    - Transaction creator added for genesis block

--- bootstrap/src/main/kotlin/jp/co/soramitsu/bootstrap/genesis/d3/D3TestGenesisFactory.kt
+++ bootstrap/src/main/kotlin/jp/co/soramitsu/bootstrap/genesis/d3/D3TestGenesisFactory.kt
@@ -13,6 +13,8 @@ import java.util.*
 
 class D3TestGenesisFactory : GenesisInterface {
 
+    private val transactionCreatorId = "notary@notary"
+
     private val zeroPubKey = "0000000000000000000000000000000000000000000000000000000000000000"
     override fun getAccountsForConfiguration(peersCount: Int): List<AccountPrototype> {
         val activeAccounts = D3TestContext.d3neededAccounts.filter { !it.passive }
@@ -32,7 +34,7 @@ class D3TestGenesisFactory : GenesisInterface {
         blockVersion: String
     ): String {
         val transactionBuilder = Transaction.builder(null)
-
+        transactionBuilder.setCreatorAccountId(transactionCreatorId)
         createPeers(peers, transactionBuilder)
         createRoles(transactionBuilder)
         createDomains(transactionBuilder)
