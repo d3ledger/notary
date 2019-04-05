92e570d3f7bb172402f91a05d085c1697c104dcc
Bogdan Mingela <freekilluau@gmail.com>
2019-04-05 15:09:51 +0300

    Added nonexistent accounts key checking (#485)

--- bootstrap/src/main/kotlin/jp/co/soramitsu/bootstrap/genesis/d3/D3TestContext.kt
+++ bootstrap/src/main/kotlin/jp/co/soramitsu/bootstrap/genesis/d3/D3TestContext.kt
@@ -3,6 +3,7 @@ package jp.co.soramitsu.bootstrap.genesis.d3
 import iroha.protocol.Primitive
 import jp.co.soramitsu.bootstrap.changelog.ChangelogInterface
 import jp.co.soramitsu.bootstrap.dto.AccountPrototype
+import jp.co.soramitsu.bootstrap.dto.NoAccountPrototype
 import jp.co.soramitsu.bootstrap.dto.PassiveAccountPrototype
 import jp.co.soramitsu.bootstrap.dto.PeersCountDependentAccountPrototype
 import jp.co.soramitsu.iroha.java.TransactionBuilder
@@ -99,7 +100,8 @@ object D3TestContext {
         AccountPrototype("admin", "notary", listOf("admin")),
         AccountPrototype("sora", "sora", listOf("sora")),
         AccountPrototype("brvs", "brvs"),
-        PassiveAccountPrototype("client_account", "notary")
+        PassiveAccountPrototype("client_account", "notary"),
+        NoAccountPrototype("registration_service_primary", "notary")
     )
 
 
