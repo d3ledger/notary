f7862cfe0329eeed7deb0e5542408211440524da
Alexey Chernyshov <alexey.n.chernyshov@gmail.com>
2019-06-17 16:38:24 +0300

    fix transfer amount
    Signed-off-by: Alexey Chernyshov <alexey.n.chernyshov@gmail.com>

--- notary-sora-integration-test/src/integration-test/kotlin/integration/sora/SoraIntegrationTest.kt
+++ notary-sora-integration-test/src/integration-test/kotlin/integration/sora/SoraIntegrationTest.kt
@@ -106,7 +106,7 @@ class SoraIntegrationTest {
 
         assertEquals(200, res.statusCode)
 
-        integrationHelper.addIrohaAssetTo(aliceClientId, xorAsset, "1334")
+        integrationHelper.addIrohaAssetTo(aliceClientId, xorAsset, "12")
 
         integrationHelper.transferAssetIrohaFromClient(
             aliceClientId,
@@ -115,7 +115,7 @@ class SoraIntegrationTest {
             bobClientId,
             xorAsset,
             "descr",
-            "1330"
+            "12"
         )
 
         assertEquals(
@@ -123,7 +123,7 @@ class SoraIntegrationTest {
             integrationHelper.getIrohaAccountBalance(aliceClientId, xorAsset)
         )
         assertEquals(
-            "1330",
+            "12",
             integrationHelper.getIrohaAccountBalance(bobClientId, xorAsset)
         )
     }
