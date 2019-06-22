2161d07772c5ae26ccd115115e180d872793b5f3
Alexey Chernyshov <alexey.n.chernyshov@gmail.com>
2019-06-22 21:17:24 +0300

    remove logic from expansion
    Signed-off-by: Alexey Chernyshov <alexey.n.chernyshov@gmail.com>

--- notary-commons/src/main/kotlin/com/d3/commons/expansion/ServiceExpansion.kt
+++ notary-commons/src/main/kotlin/com/d3/commons/expansion/ServiceExpansion.kt
@@ -1,16 +1,12 @@
 package com.d3.commons.expansion
 
-import com.d3.commons.model.IrohaCredential
-import com.d3.commons.sidechain.iroha.consumer.MultiSigIrohaConsumer
 import com.d3.commons.util.irohaUnEscape
-import com.d3.commons.util.unHex
 import com.google.gson.Gson
 import iroha.protocol.BlockOuterClass
 import iroha.protocol.Commands
 import jp.co.soramitsu.bootstrap.changelog.ChangelogInterface
 import jp.co.soramitsu.bootstrap.changelog.ExpansionDetails
 import jp.co.soramitsu.iroha.java.IrohaAPI
-import jp.co.soramitsu.iroha.java.Transaction
 import mu.KLogging
 
 /**
@@ -28,13 +24,11 @@ class ServiceExpansion(
     /**
      * Expands given accounts quorum
      * @param block - Iroha blocks that is used to check if there is a 'expansion' transaction made by superuser
-     * @param accountsToExpand - accounts that will be expanded
-     * @param additionalLogic - additional expansion logic
+     * @param expansionLogic - additional expansion logic
      */
     fun expand(
         block: BlockOuterClass.Block,
-        accountsToExpand: List<IrohaCredential>,
-        additionalLogic: (ExpansionDetails) -> Unit = {}
+        expansionLogic: (ExpansionDetails) -> Unit = {}
     ) {
         block.blockV1.payload.transactionsList
             // Get superuser transactions
@@ -42,7 +36,8 @@ class ServiceExpansion(
             // Get commands
             .flatMap { tx -> tx.payload.reducedPayload.commandsList }
             // Get set account details
-            .filter { command -> command.hasSetAccountDetail() }.map { command -> command.setAccountDetail }
+            .filter { command -> command.hasSetAccountDetail() }
+            .map { command -> command.setAccountDetail }
             // Get expansion details
             .filter { setAccountDetail -> isExpansionEvent(setAccountDetail) }
             .map { setAccountDetail ->
@@ -52,72 +47,9 @@ class ServiceExpansion(
                 )
             }
             .forEach { expansionDetails ->
-                // Get account that needs expansion
-                val account =
-                    accountsToExpand.find { account -> account.accountId == expansionDetails.accountIdToExpand }
-                if (account != null) {
-                    // Expand account
-                    expandAccount(account, expansionDetails, block.blockV1.payload.createdTime)
-                    additionalLogic(expansionDetails)
-                } else {
-                    logger.warn("No account with id ${expansionDetails.accountIdToExpand} was found. Cannot expand.")
-                }
+                expansionLogic(expansionDetails)
             }
     }
-
-    /**
-     * Expands given account
-     * @param account - account to expand
-     * @param expansionDetails - details of expansion
-     * @param txTime - time of 'expansion' transaction. Must be the same on all the nodes.
-     */
-    private fun expandAccount(
-        account: IrohaCredential,
-        expansionDetails: ExpansionDetails,
-        txTime: Long
-    ) {
-        val consumer = MultiSigIrohaConsumer(account, irohaAPI)
-        consumer.send(
-            createExpandingTransaction(
-                account,
-                expansionDetails,
-                txTime,
-                consumer.getConsumerQuorum().get()
-            )
-        ).fold(
-            { txHash ->
-                logger.info(
-                    "Expanding transaction for account ${account.accountId} has been sent." +
-                            " Expansion details $expansionDetails. Tx hash $txHash"
-                )
-            },
-            { ex ->
-                logger.error(
-                    "Cannot expand account ${account.accountId}." +
-                            " Expansion details $expansionDetails",
-                    ex
-                )
-            })
-    }
-
-    /**
-     * Creates 'expansion' transaction
-     * @param account - account to expand
-     * @param expansionDetails - details of expansion
-     * @param txTime - time of transaction
-     * @param currentQuorum - expanded account current quorum
-     */
-    private fun createExpandingTransaction(
-        account: IrohaCredential,
-        expansionDetails: ExpansionDetails, txTime: Long,
-        currentQuorum: Int
-    ) =
-        Transaction.builder(account.accountId)
-            .addSignatory(account.accountId, String.unHex(expansionDetails.publicKey.toLowerCase()))
-            .setAccountQuorum(account.accountId, expansionDetails.quorum)
-            .setQuorum(currentQuorum)
-            .setCreatedTime(txTime)
-            .build()
 
     /**
      * Checks if command is 'expansion' command
