524c718da437c7dffb2bd097dd15bc1f55c07924
Anton Dolgopolov <dolgopolov@soramitsu.co.jp>
2019-07-16 14:35:03 +0300

    Fixed function

--- notary-commons/src/main/kotlin/com/d3/commons/sidechain/iroha/util/RequestHelper.kt
+++ notary-commons/src/main/kotlin/com/d3/commons/sidechain/iroha/util/RequestHelper.kt
@@ -8,6 +8,7 @@ package com.d3.commons.sidechain.iroha.util
 import iroha.protocol.BlockOuterClass
 import iroha.protocol.Commands
 import iroha.protocol.QryResponses
+import iroha.protocol.TransactionOuterClass
 
 /**
  * Returns pretty formatted error message. May be used in exceptions.
@@ -46,4 +47,21 @@ fun getCreateAccountCommands(block: BlockOuterClass.Block): List<Commands.Comman
 fun getTransferCommands(block: BlockOuterClass.Block): List<Commands.Command> {
     return block.blockV1.payload.transactionsList.flatMap { tx -> tx.payload.reducedPayload.commandsList }
         .filter { command -> command.hasTransferAsset() }
+}
+
+/**
+ * Return all withdrawal transactions from Iroha block
+ * @param block - Iroha block
+ * @param withdrawalAccount - account that is used to perform withdrawals
+ * @return list full of "transfer asset" transactions
+ */
+fun getTransferTransactions(
+    block: BlockOuterClass.Block,
+    withdrawalAccount: String
+): List<TransactionOuterClass.Transaction> {
+    return block.blockV1.payload.transactionsList
+        .filter { tx ->
+            tx.payload.reducedPayload.commandsList
+                .any { it.hasTransferAsset() && it.transferAsset.destAccountId == withdrawalAccount }
+        }
 }
