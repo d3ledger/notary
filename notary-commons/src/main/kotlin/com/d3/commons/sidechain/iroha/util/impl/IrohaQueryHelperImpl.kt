66fa374f9fc9ede58484907f0adf34016f863da8
Alexey <Alexey.N.Chernyshov@gmail.com>
2019-08-02 11:23:23 +0300

    Feature/bump to iroha 1.1.0 (#624)
    * bump Iroha version
    
    Signed-off-by: Alexey Chernyshov <alexey.n.chernyshov@gmail.com>

--- notary-commons/src/main/kotlin/com/d3/commons/sidechain/iroha/util/impl/IrohaQueryHelperImpl.kt
+++ notary-commons/src/main/kotlin/com/d3/commons/sidechain/iroha/util/impl/IrohaQueryHelperImpl.kt
@@ -8,10 +8,9 @@ package com.d3.commons.sidechain.iroha.util.impl
 import com.d3.commons.config.IrohaCredentialRawConfig
 import com.d3.commons.model.IrohaCredential
 import com.d3.commons.sidechain.iroha.util.IrohaQueryHelper
-import com.github.kittinunf.result.Result
-import com.github.kittinunf.result.flatMap
-import com.github.kittinunf.result.map
 import com.d3.commons.util.GsonInstance
+import com.github.kittinunf.result.Result
+import com.github.kittinunf.result.map
 import com.google.gson.reflect.TypeToken
 import iroha.protocol.QryResponses
 import iroha.protocol.TransactionOuterClass
@@ -22,8 +21,10 @@ import java.util.*
 
 /**
  * The purpose of the class is to hide Iroha query implementation.
+ * @param queryAPI - query API by Iroha-Java library
+ * @param pageSize - number of items per one Iroha query
  */
-class IrohaQueryHelperImpl(val queryAPI: QueryAPI) : IrohaQueryHelper {
+class IrohaQueryHelperImpl(val queryAPI: QueryAPI, val pageSize: Int = 100) : IrohaQueryHelper {
     constructor(irohaAPI: IrohaAPI, accountId: String, keyPair: KeyPair) : this(
         QueryAPI(
             irohaAPI,
@@ -47,18 +48,77 @@ class IrohaQueryHelperImpl(val queryAPI: QueryAPI) : IrohaQueryHelper {
 
     private val gson = GsonInstance.get()
 
+    private fun getPaginatedAccountDetails(
+        storageAccountId: String,
+        writerAccountId: String,
+        key: String?
+    ): Result<Map<String, Map<String, String>>, Exception> {
+        return Result.of {
+            var lastWriter: String? = null
+            var lastKey: String? = null
+
+            val detailsMap = mutableMapOf<String, MutableMap<String, String>>()
+
+            do {
+                val response = queryAPI.getAccountDetails(
+                    storageAccountId,
+                    writerAccountId,
+                    key,
+                    pageSize,
+                    lastWriter,
+                    lastKey
+                )
+                lastWriter = response.nextRecordId.writer
+                lastKey = response.nextRecordId.key
+
+                val toAdd = parseAccountDetailsJson(response.detail).get()
+                toAdd.entries.forEach { (writer, details) ->
+                    detailsMap.merge(writer, details.toMutableMap()) { oldVal, newVal ->
+                        oldVal.putAll(newVal)
+                        oldVal
+                    }
+                }
+            } while (response.hasNextRecordId())
+
+            detailsMap
+        }
+    }
+
+    private fun getPaginatedAccountAssets(
+        accountId: String
+    ): Result<Map<String, String>, Exception> {
+        return Result.of {
+            var lastAssetId: String? = null
+
+            val assetMap = mutableMapOf<String, String>()
+
+            do {
+                val response = queryAPI.getAccountAssets(
+                    accountId,
+                    pageSize,
+                    lastAssetId
+                )
+                lastAssetId = response.nextAssetId
+                assetMap.putAll(response.accountAssetsList.associate { asset ->
+                    asset.assetId to asset.balance
+                })
+            } while (response.nextAssetId != null && response.nextAssetId != "")
+
+            assetMap
+        }
+    }
+
     /**
      * Deserialise JSON string to Map<String, String>
      *
      * @param jsonStr JSON as String
      * @return desereialized details as (writer -> (key -> value))
      */
-    private fun parseAccountDetailsJson(jsonStr: String): Result<Map<String, Map<String, String>>, Exception> {
-        return Result.of {
-            val responseType = object : TypeToken<Map<String, Map<String, String>>>() {}.getType()
+    private fun parseAccountDetailsJson(jsonStr: String): Result<Map<String, Map<String, String>>, Exception> =
+        Result.of {
+            val responseType = object : TypeToken<Map<String, Map<String, String>>>() {}.type
             gson.fromJson<Map<String, Map<String, String>>>(jsonStr, responseType)
         }
-    }
 
     /** {@inheritDoc} */
     override fun getAccount(accountId: String): Result<QryResponses.AccountResponse, Exception> {
@@ -66,49 +126,36 @@ class IrohaQueryHelperImpl(val queryAPI: QueryAPI) : IrohaQueryHelper {
     }
 
     /** {@inheritDoc} */
-    override fun getSignatories(accountId: String): Result<List<String>, Exception> {
-        return Result.of {
-            val sigs = queryAPI.getSignatories(accountId)
-            sigs.keysList
-        }
-    }
+    override fun getSignatories(accountId: String): Result<List<String>, Exception> =
+        Result.of { queryAPI.getSignatories(accountId).keysList }
 
     /** {@inheritDoc} */
     override fun getAccountDetails(
         storageAccountId: String,
         writerAccountId: String
-    ): Result<Map<String, String>, Exception> {
-        return Result.of { queryAPI.getAccountDetails(storageAccountId, writerAccountId, null) }
-            .flatMap { str -> parseAccountDetailsJson(str) }
+    ): Result<Map<String, String>, Exception> =
+        getPaginatedAccountDetails(storageAccountId, writerAccountId, null)
             .map { details ->
                 if (details[writerAccountId] == null)
                     emptyMap()
                 else
                     details.getOrDefault(writerAccountId, emptyMap())
             }
-    }
 
     /** {@inheritDoc} */
     override fun getAccountDetails(
         storageAccountId: String,
         writerAccountId: String,
         key: String
-    ): Result<Optional<String>, Exception> {
-        return Result.of { queryAPI.getAccountDetails(storageAccountId, writerAccountId, null) }
-            .flatMap { str -> parseAccountDetailsJson(str) }
+    ): Result<Optional<String>, Exception> =
+        getPaginatedAccountDetails(storageAccountId, writerAccountId, key)
             .map { details ->
-                val result = if (details[writerAccountId] == null) {
-                    null
-                } else {
-                    details.getOrDefault(writerAccountId, emptyMap())[key]
-                }
-                Optional.ofNullable(result)
+                Optional.ofNullable(details.getOrDefault(writerAccountId, emptyMap()).get(key))
             }
-    }
 
     /** {@inheritDoc} */
-    override fun getAssetPrecision(assetId: String): Result<Int, Exception> {
-        return Result.of { queryAPI.getAssetInfo(assetId) }
+    override fun getAssetPrecision(assetId: String): Result<Int, Exception> =
+        Result.of { queryAPI.getAssetInfo(assetId) }
             .map { queryResponse ->
                 val asset = queryResponse.asset
                 if (asset.assetId.isNullOrEmpty()) {
@@ -116,55 +163,37 @@ class IrohaQueryHelperImpl(val queryAPI: QueryAPI) : IrohaQueryHelper {
                 }
                 asset.precision
             }
-    }
 
     /** {@inheritDoc} */
-    override fun getAccountAssets(accountId: String): Result<Map<String, String>, Exception> {
-        return Result.of {
-            queryAPI.getAccountAssets(accountId).accountAssetsList.associate { asset ->
-                asset.assetId to asset.balance
-            }
-        }
-    }
+    override fun getAccountAssets(accountId: String): Result<Map<String, String>, Exception> =
+        getPaginatedAccountAssets(accountId)
 
     /** {@inheritDoc} */
-    override fun getAccountAsset(
-        accountId: String,
-        assetId: String
-    ): Result<String, Exception> {
-        return Result.of { queryAPI.getAccountAssets(accountId) }
-            .map { queryResponse ->
-                val accountAsset = queryResponse.accountAssetsList
-                    .find { it.assetId == assetId }
-
-                accountAsset?.balance ?: "0"
-            }
-    }
+    override fun getAccountAsset(accountId: String, assetId: String): Result<String, Exception> =
+        getPaginatedAccountAssets(accountId)
+            .map { it.getOrDefault(assetId, "0") }
 
     /** {@inheritDoc} */
-    override fun getBlock(height: Long): Result<QryResponses.BlockResponse, Exception> {
-        return Result.of { queryAPI.getBlock(height) }
-    }
+    override fun getBlock(height: Long): Result<QryResponses.BlockResponse, Exception> =
+        Result.of { queryAPI.getBlock(height) }
+
 
     /** {@inheritDoc} */
-    override fun getAccountQuorum(acc: String): Result<Int, Exception> {
-        return getAccount(acc).map { queryResponse ->
+    override fun getAccountQuorum(acc: String): Result<Int, Exception> =
+        getAccount(acc).map { queryResponse ->
             queryResponse.account.quorum
         }
-    }
 
     /** {@inheritDoc} */
-    override fun getTransactions(hashes: Iterable<String>): Result<QryResponses.TransactionsResponse, Exception> {
-        return Result.of { queryAPI.getTransactions(hashes) }
-    }
+    override fun getTransactions(hashes: Iterable<String>): Result<QryResponses.TransactionsResponse, Exception> =
+        Result.of { queryAPI.getTransactions(hashes) }
 
     /** {@inheritDoc} */
-    override fun getSingleTransaction(hash: String): Result<TransactionOuterClass.Transaction, Exception> {
-        return getTransactions(listOf(hash)).map { queryResponse ->
+    override fun getSingleTransaction(hash: String): Result<TransactionOuterClass.Transaction, Exception> =
+        getTransactions(listOf(hash)).map { queryResponse ->
             if (queryResponse.transactionsCount == 0)
                 throw Exception("There is no transactions.")
 
             queryResponse.transactionsList[0]
         }
-    }
 }
--- notary-commons/src/test/kotlin/com/d3/commons/sidechain/iroha/util/impl/IrohaQueryHelperImplTest.kt
+++ notary-commons/src/test/kotlin/com/d3/commons/sidechain/iroha/util/impl/IrohaQueryHelperImplTest.kt
@@ -5,16 +5,157 @@
 
 package com.d3.commons.sidechain.iroha.util.impl
 
-import com.nhaarman.mockitokotlin2.doReturn
-import com.nhaarman.mockitokotlin2.eq
-import com.nhaarman.mockitokotlin2.isNull
-import com.nhaarman.mockitokotlin2.mock
-import iroha.protocol.QryResponses
+import iroha.protocol.BlockOuterClass
+import iroha.protocol.Primitive
+import jp.co.soramitsu.crypto.ed25519.Ed25519Sha3
 import jp.co.soramitsu.iroha.java.QueryAPI
+import jp.co.soramitsu.iroha.java.Transaction
+import jp.co.soramitsu.iroha.testcontainers.IrohaContainer
+import jp.co.soramitsu.iroha.testcontainers.PeerConfig
+import jp.co.soramitsu.iroha.testcontainers.detail.GenesisBlockBuilder
+import org.junit.jupiter.api.AfterAll
 import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.TestInstance
+import java.util.*
 import kotlin.test.assertEquals
+import kotlin.test.assertTrue
 
+@TestInstance(TestInstance.Lifecycle.PER_CLASS)
 class IrohaQueryHelperImplTest {
+
+    private val crypto = Ed25519Sha3()
+    private val peerKeypair = crypto.generateKeypair()
+    private val rolename = "princess"
+    private val domain = "duloc"
+    private val accountName = "fiona"
+    private val accountKeypair = crypto.generateKeypair()
+    private val accountId = "$accountName@$domain"
+    private val details = mapOf("key1" to "value1", "key2" to "value2", "key3" to "value3")
+    private val assets = mapOf("asset1" to "1", "asset2" to "2", "asset3" to "3")
+
+    val iroha = IrohaContainer().withPeerConfig(getPeerConfig())
+
+    init {
+        iroha.start()
+    }
+
+    val queryHelper =
+        IrohaQueryHelperImpl(QueryAPI(iroha.api, accountId, accountKeypair), pageSize = 2)
+
+    private fun getGenesisBlock(): BlockOuterClass.Block {
+        val blockBuilder = GenesisBlockBuilder()
+            .addTransaction(
+                Transaction.builder(null)
+                    .addPeer("0.0.0.0:10001", peerKeypair.public)
+                    .createRole(
+                        rolename,
+                        Arrays.asList(
+                            Primitive.RolePermission.can_get_all_acc_detail,
+                            Primitive.RolePermission.can_set_detail,
+                            Primitive.RolePermission.can_get_all_acc_ast
+                        )
+                    ).createDomain(domain, rolename)
+                    .createAccount(accountName, domain, accountKeypair.getPublic())
+                    .build() // returns ipj model Transaction
+                    .build() // returns unsigned protobuf Transaction
+            )
+
+        val tx = Transaction.builder(accountId)
+        details.forEach { (key, value) ->
+            tx.setAccountDetail(accountId, key, value)
+        }
+        assets.forEach { (assetName, amount) ->
+            tx.createAsset(assetName, domain, 1)
+                .addAssetQuantity("$assetName#$domain", amount)
+        }
+
+        return blockBuilder.addTransaction(
+            tx.build().build()
+        ).build()
+    }
+
+    fun getPeerConfig(): PeerConfig {
+        val config = PeerConfig.builder()
+            .genesisBlock(getGenesisBlock())
+            .build()
+
+        // don't forget to add peer keypair to config
+        config.withPeerKeyPair(peerKeypair)
+
+        return config
+    }
+
+    @AfterAll
+    fun dropDown() {
+        iroha.stop()
+    }
+
+    /**
+     * @given queryHelper and Iroha populated with details
+     * @when query account for all details
+     * @then all account details are returned
+     */
+    @Test
+    fun getAccountDetailsTest() {
+        val actual = queryHelper.getAccountDetails(accountId, accountId).get()
+        assertEquals(details, actual)
+    }
+
+    /**
+     * @given queryHelper and Iroha populated with details
+     * @when query details for an account that doesn't exist
+     * @then empty map is returned
+     */
+    @Test
+    fun getAccountDetailsEmptyAccountTest() {
+        val actual = queryHelper.getAccountDetails(accountId, "empty@account").get()
+        assertTrue(actual.isEmpty())
+    }
+
+    /**
+     * @given queryHelper and Iroha populated with details
+     * @when query account detail for "key1"
+     * @then "value1" is returned
+     */
+    @Test
+    fun getAccountDetailTest() {
+        val actual = queryHelper.getAccountDetails(accountId, accountId, "key1").get()
+        assertTrue(actual.isPresent)
+        assertEquals(details["key1"], actual.get())
+    }
+
+    /**
+     * @given queryHelper and Iroha populated with details
+     * @when query account for key that doesn't exist
+     * @then optional null is returned
+     */
+    @Test
+    fun getNonExistAccountDetailsTest() {
+        val actual = queryHelper.getAccountDetails(accountId, accountId, "nonexist_key").get()
+        assertTrue(!actual.isPresent)
+    }
+
+    /**
+     * @given queryHelper and Iroha populated with assets
+     * @when query account asset "asset1"
+     * @then returns amount "1" of "asset1"
+     */
+    @Test
+    fun getAccountAssetTest() {
+        val res = queryHelper.getAccountAsset(accountId, "asset1#$domain").get()
+        assertEquals("1", res)
+    }
+
+    /**
+     * @given queryHelper and Iroha populated with assets
+     * @when query account assets
+     * @then list of assets belonging to account is returned
+     */
+    @Test
+    fun getAccountAssetsTest() {
+        val actual = queryHelper.getAccountAssets(accountId).get()
+        assertEquals(assets.mapKeys { (assetName, _) -> "$assetName#$domain" }, actual)
+    }
 
     /**
      * @given queryHelper
@@ -22,77 +163,9 @@ class IrohaQueryHelperImplTest {
      * @then return "0"
      */
     @Test
-    fun getNonexistentAccountAssetTest() {
-        val clientId = "client@domain"
-        val assetId = "nonexist#domain"
-
-        val emptyResponse = QryResponses.AccountAssetResponse.newBuilder().build()
-        val queryAPI = mock<QueryAPI>() {
-            on { getAccountAssets(eq(clientId)) } doReturn emptyResponse
-        }
-
-        val queryHelper = IrohaQueryHelperImpl(queryAPI)
-
-        val res = queryHelper.getAccountAsset(clientId, assetId)
-        assertEquals("0", res.get())
+    fun getAccountNonExistAsset() {
+        val res = queryHelper.getAccountAsset(accountId, "nonexist#$domain").get()
+        assertEquals("0", res)
     }
 
-    /**
-     * @given queryHelper
-     * @when query details of "writer#domain" that does not exist
-     * @then return null
-     */
-    @Test
-    fun getEmptyAccountData() {
-        val storageClientId = "client@domain"
-        val writerClientId = "writer@domain"
-
-        val emptyResponse = "{\"writer@domain\" : null}"
-        val queryAPI = mock<QueryAPI>() {
-            on {
-                getAccountDetails(
-                    eq(storageClientId),
-                    eq(writerClientId),
-                    isNull()
-                )
-            } doReturn emptyResponse
-        }
-
-        val queryHelper = IrohaQueryHelperImpl(queryAPI)
-
-        val res = queryHelper.getAccountDetails(storageClientId, writerClientId)
-        assertEquals(emptyMap(), res.get())
-    }
-
-    /**
-     * @given queryHelper
-     * @when query details of "account@a_domain"
-     * @then return details
-     */
-    @Test
-    fun getAccountData() {
-        val storageClientId = "client@domain"
-        val writerA = "account@a_domain"
-
-        val emptyResponse = "{\n" +
-                "    \"account@a_domain\": {\n" +
-                "        \"age\": 18,\n" +
-                "        \"hobbies\": \"crypto\"\n" +
-                "    }\n" +
-                "}"
-        val queryAPI = mock<QueryAPI>() {
-            on {
-                getAccountDetails(
-                    eq(storageClientId),
-                    eq(writerA),
-                    isNull()
-                )
-            } doReturn emptyResponse
-        }
-
-        val queryHelper = IrohaQueryHelperImpl(queryAPI)
-
-        val resA = queryHelper.getAccountDetails(storageClientId, writerA)
-        assertEquals(mapOf("age" to "18", "hobbies" to "crypto"), resA.get())
-    }
 }
