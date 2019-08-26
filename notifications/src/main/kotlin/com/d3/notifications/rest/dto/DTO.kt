70b980c529cd1397babdb1e8ec63811dea6bd3c3
dolgopolovwork <37849469+dolgopolovwork@users.noreply.github.com>
2019-08-26 13:00:10 +0300

    Fixed SMTP message parsing (#646)

--- notifications/src/main/kotlin/com/d3/notifications/rest/dto/DTO.kt
+++ notifications/src/main/kotlin/com/d3/notifications/rest/dto/DTO.kt
@@ -25,5 +25,18 @@ fun mapDumbsterMessage(mail: SmtpMessage) = DumbsterMessage(
     from = mail.getHeaderValue(FROM_HEADER),
     to = mail.getHeaderValue(TO_HEADER),
     subject = mail.getHeaderValue(SUBJECT_HEADER),
-    message = mail.body
+    message = parseSMTPMessageBody(mail)
 )
+
+/**
+ * Takes text from SMTP message
+ * @param mail - SMTP message
+ * @return plain text with no SMTP-related headers
+ */
+private fun parseSMTPMessageBody(mail: SmtpMessage): String {
+    val message = mail.body
+    return message.substring(
+        message.indexOf('\n') + 1,
+        message.lastIndexOf("------")
+    )
+}
