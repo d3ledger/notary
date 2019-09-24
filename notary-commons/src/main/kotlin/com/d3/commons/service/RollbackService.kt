c862dcf06552eea3e9b1f52a2c7871a704d1708e
Anton Dolgopolov <dolgopolov@soramitsu.co.jp>
2019-09-24 19:17:48 +0300

    New function createRollbackTransaction

--- notary-commons/src/main/kotlin/com/d3/commons/service/RollbackService.kt
+++ notary-commons/src/main/kotlin/com/d3/commons/service/RollbackService.kt
@@ -11,6 +11,7 @@ import com.d3.commons.sidechain.iroha.consumer.IrohaConsumer
 import com.github.kittinunf.result.Result
 import com.github.kittinunf.result.flatMap
 import com.github.kittinunf.result.map
+import iroha.protocol.Commands
 import jp.co.soramitsu.iroha.java.Transaction
 import java.math.BigDecimal
 
@@ -31,30 +32,45 @@ class RollbackService(private val withdrawalConsumer: IrohaConsumer) {
     ): Result<String, Exception> {
         return withdrawalConsumer.getConsumerQuorum()
             .map { quorum ->
-                val transaction = Transaction
-                    .builder(withdrawalConsumer.creator)
-                    .setCreatedTime(withdrawalDetails.withdrawalTime)
-                    .setQuorum(quorum)
-                    .transferAsset(
-                        withdrawalConsumer.creator,
-                        withdrawalDetails.srcAccountId,
-                        withdrawalDetails.withdrawalAssetId,
-                        "$ROLLBACK_DESCRIPTION. $reason".take(64).toLowerCase(),
-                        withdrawalDetails.withdrawalAmount
-                    )
-
-                if (withdrawalDetails.feeAmount > BigDecimal.ZERO)
-                    transaction.transferAsset(
-                        withdrawalConsumer.creator,
-                        withdrawalDetails.srcAccountId,
-                        withdrawalDetails.feeAssetId,
-                        FEE_ROLLBACK_DESCRIPTION,
-                        withdrawalDetails.feeAmount
-                    )
-
-                transaction.build()
+                createRollbackTransaction(withdrawalDetails, reason, quorum)
             }.flatMap { transaction ->
                 withdrawalConsumer.send(transaction)
             }
+    }
+
+    /**
+     * Creates rollback Iroha transaction
+     * @param withdrawalDetails - details with info about withdrawal and fee
+     * @param reason - text description of rollback
+     * @param quorum - transaction quorum
+     * @return rollback Iroha transaction
+     */
+    fun createRollbackTransaction(
+        withdrawalDetails: WithdrawalFinalizationDetails,
+        reason: String,
+        quorum: Int
+    ): Transaction {
+        val transaction = Transaction
+            .builder(withdrawalConsumer.creator)
+            .setCreatedTime(withdrawalDetails.withdrawalTime)
+            .setQuorum(quorum)
+            .transferAsset(
+                withdrawalConsumer.creator,
+                withdrawalDetails.srcAccountId,
+                withdrawalDetails.withdrawalAssetId,
+                "$ROLLBACK_DESCRIPTION. $reason".take(64).toLowerCase(),
+                withdrawalDetails.withdrawalAmount
+            )
+
+        if (withdrawalDetails.feeAmount > BigDecimal.ZERO)
+            transaction.transferAsset(
+                withdrawalConsumer.creator,
+                withdrawalDetails.srcAccountId,
+                withdrawalDetails.feeAssetId,
+                FEE_ROLLBACK_DESCRIPTION,
+                withdrawalDetails.feeAmount
+            )
+
+        return transaction.build()
     }
 }
